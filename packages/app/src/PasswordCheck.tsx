2a130a5e3fea261c1930db9df950a08abdf9987f
Justin George <justin.george@gmail.com>
2023-11-15 09:36:18 -0800

    Password Check: replace SVG with html icon

--- packages/app/src/PasswordCheck.tsx
+++ packages/app/src/PasswordCheck.tsx
@@ -65,28 +65,6 @@ export const CheckOrX = ({
   );
 };
 
-const Check = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    fill="currentColor"
-    className="bi bi-check2"
-    viewBox="0 0 16 16"
-  >
-    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
-  </svg>
-);
+const Check = () => <i className={'bi bi-check2'}></i>;
 
-const XShape = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    fill="currentColor"
-    className="bi bi-x"
-    viewBox="0 0 16 16"
-  >
-    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-  </svg>
-);
+const XShape = () => <i className={'bi bi-x'}></i>;
