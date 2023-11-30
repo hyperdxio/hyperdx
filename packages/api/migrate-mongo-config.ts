96b958a75ec57872684985b02867f3bede23e286
Warren <5959690+wrn14897@users.noreply.github.com>
2023-11-29 23:22:36 -0800

    style: remove warning

--- packages/api/migrate-mongo-config.ts
+++ packages/api/migrate-mongo-config.ts
@@ -7,8 +7,8 @@ export = {
     databaseName: 'hyperdx',
 
     options: {
-      useNewUrlParser: true, // removes a deprecation warning when connecting
-      useUnifiedTopology: true, // removes a deprecating warning when connecting
+      // useNewUrlParser: true, // removes a deprecation warning when connecting
+      // useUnifiedTopology: true, // removes a deprecating warning when connecting
       //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
       //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
     },
