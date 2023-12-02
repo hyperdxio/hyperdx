e60e1d909522fba55ef477bab023b392f81c5d2f
Warren <5959690+wrn14897@users.noreply.github.com>
2023-12-01 16:51:36 -0800

    style: cleanup migration sqls

--- packages/api/migrations/ch/000001_add_is_delta_n_is_monotonic_fields_to_metric_stream_table.down.sql
+++ packages/api/migrations/ch/000001_add_is_delta_n_is_monotonic_fields_to_metric_stream_table.down.sql
@@ -1,5 +1,3 @@
 ALTER TABLE default.metric_stream DROP COLUMN is_delta;
 
---migration:split
-
 ALTER TABLE default.metric_stream DROP COLUMN is_monotonic;
--- packages/api/migrations/ch/000001_add_is_delta_n_is_monotonic_fields_to_metric_stream_table.up.sql
+++ packages/api/migrations/ch/000001_add_is_delta_n_is_monotonic_fields_to_metric_stream_table.up.sql
@@ -1,5 +1,3 @@
 ALTER TABLE default.metric_stream ADD COLUMN is_delta Boolean CODEC(Delta, ZSTD(1));
 
---migration:split
-
 ALTER TABLE default.metric_stream ADD COLUMN is_monotonic Boolean CODEC(Delta, ZSTD(1));
