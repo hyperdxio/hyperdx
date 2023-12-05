2157811e04ca38d451e19648104858e05e0e35d0
Justin George <justin@hyperdx.io>
2023-12-05 15:35:32 -0800

    Skip trying to generate Metric chart when metric name is empty

--- packages/app/src/api.ts
+++ packages/app/src/api.ts
@@ -120,6 +120,7 @@ const api = {
     const endTime = endDate.getTime();
 
     // FIXME: pass metric name and type separately
+    if (!name.includes(' - ')) return;
     const [metricName, metricDataType] = name.split(' - ');
 
     return useQuery<any, Error>({
