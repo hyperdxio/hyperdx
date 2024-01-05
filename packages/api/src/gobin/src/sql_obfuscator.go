5c984c609285d74db59c8e75faf17a5a3363a7cc
Justin George <justin@hyperdx.io>
2024-01-04 23:56:11 -0800

    include source directly in gobin for now

---
+++ packages/api/src/gobin/src/sql_obfuscator.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/DataDog/go-sqllexer"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		query, err := reader.ReadString('\n')
+		if err != nil {
+			// write to stderr
+			fmt.Fprintf(os.Stderr, "error: %s\n", err)
+			os.Exit(1)
+		}
+		stripped_string := strings.Trim(query, " \t\r\n")
+		if stripped_string == "" {
+			// skip empty entries
+			continue
+		}
+
+		normalizer := sqllexer.NewNormalizer(
+			sqllexer.WithCollectComments(false),
+			sqllexer.WithCollectCommands(false),
+			sqllexer.WithCollectTables(false),
+			sqllexer.WithKeepSQLAlias(false),
+		)
+
+		normalized, _, _ := normalizer.Normalize(query)
+		obfuscator := sqllexer.NewObfuscator()
+		obfuscated := obfuscator.Obfuscate(normalized)
+		fmt.Printf("%s\n", obfuscated)
+	}
+}
