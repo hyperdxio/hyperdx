450693f7209709062e5ad33bbdaf822a8848e3ed
Shorpo <149748269+svc-shorpo@users.noreply.github.com>
2024-01-04 18:01:42 -0700

    feat: Placeholder page for Service Dashboard (#180)
    ![Screenshot 2024-01-03 at 9 30 34 PM](https://github.com/hyperdxio/hyperdx/assets/149748269/b3ec8bbb-2f80-43a2-b4be-b8ab6fd63642)

---
+++ .changeset/thin-carpets-tease.md
@@ -0,0 +1,5 @@
+---
+'@hyperdx/app': patch
+---
+
+Placeholder page for Service Dashboard
---
+++ packages/app/pages/services.tsx
@@ -0,0 +1,3 @@
+import ServiceDashboardPage from '../src/ServiceDashboardPage';
+
+export default ServiceDashboardPage;
