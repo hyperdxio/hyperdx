fe95f38fbd24c3631cb5870c17089be71771981b
Warren <5959690+wrn14897@users.noreply.github.com>
2024-01-05 13:54:26 -0800

    test: missing attributes

--- packages/api/src/routers/api/__tests__/chart.test.ts
+++ packages/api/src/routers/api/__tests__/chart.test.ts
@@ -87,4 +87,56 @@ Object {
 }
 `);
   });
+
+  it('GET /chart/services (missing custom attributes)', async () => {
+    const now = Date.now();
+    const { agent, team } = await getLoggedInAgent(server);
+
+    await clickhouse.bulkInsertTeamLogStream(
+      team.logStreamTableVersion,
+      team.id,
+      [
+        buildEvent({
+          timestamp: now - ms('30m'),
+          service: 'service1',
+          'k8s.namespace.name': 'namespace1',
+          'k8s.pod.uid': 'uid1',
+        }),
+        buildEvent({
+          timestamp: now - ms('1d'),
+          service: 'service1',
+          'k8s.namespace.name': 'namespace1',
+          'k8s.pod.uid': 'uid2',
+        }),
+        buildEvent({
+          timestamp: now - ms('1h'),
+          service: 'service2',
+          'k8s.namespace.name': 'namespace2',
+          'k8s.pod.uid': 'uid3',
+        }),
+      ],
+    );
+
+    const results = await agent.get('/chart/services').expect(200);
+    expect(results.body.data).toMatchInlineSnapshot(`
+Object {
+  "service1": Array [
+    Object {
+      "k8s.namespace.name": "namespace1",
+      "k8s.pod.uid": "uid1",
+    },
+    Object {
+      "k8s.namespace.name": "namespace1",
+      "k8s.pod.uid": "uid2",
+    },
+  ],
+  "service2": Array [
+    Object {
+      "k8s.namespace.name": "namespace2",
+      "k8s.pod.uid": "uid3",
+    },
+  ],
+}
+`);
+  });
 });
