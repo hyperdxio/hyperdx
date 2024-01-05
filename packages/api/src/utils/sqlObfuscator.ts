b9c97f92a058535afd573b41b73fba9d243a95c4
Justin George <justin@hyperdx.io>
2024-01-05 15:20:49 -0800

    SqlObfuscator only import ChildProcess as a type

--- packages/api/src/utils/sqlObfuscator.ts
+++ packages/api/src/utils/sqlObfuscator.ts
@@ -1,4 +1,5 @@
-import { ChildProcess, spawn } from 'child_process';
+import type { ChildProcess } from 'child_process';
+import { spawn } from 'child_process';
 
 let subprocess: ChildProcess | undefined;
 
