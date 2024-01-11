8f1ca6b1f1aa8663d4f68d22ab62af3d11a2ccfb
Shorpo <149748269+svc-shorpo@users.noreply.github.com>
2024-01-10 22:51:14 -0700

    feat: Add K8s Pod Timeline to Service Dashboard and Log Details metrics (#222)
    * Add K8s Pod Timeline to Service Dashboard and Log Details metrics
    * Minor styling tweaks
    
    ![Screenshot 2024-01-10 at 7 45 51 PM](https://github.com/hyperdxio/hyperdx/assets/149748269/9d7bd3a2-3d23-441c-adc7-26e2631ff1bd)
    
    
    ![Screenshot 2024-01-10 at 7 39 20 PM](https://github.com/hyperdxio/hyperdx/assets/149748269/2741c1f2-107e-446f-bb4f-18071f9a1bef)
    
    ![Screenshot 2024-01-10 at 7 38 43 PM](https://github.com/hyperdxio/hyperdx/assets/149748269/935b8e92-b81c-46fd-9b8b-92cb97d43ced)

---
+++ packages/app/src/components/KubeComponents.tsx
@@ -0,0 +1,155 @@
+import * as React from 'react';
+import { format, sub } from 'date-fns';
+import { Badge, Group, Text, Timeline } from '@mantine/core';
+
+import api from '../api';
+
+type KubeEvent = {
+  id: string;
+  timestamp: string;
+  severity_text?: string;
+  'object.reason'?: string;
+  'object.note'?: string;
+  'object.type'?: string;
+};
+
+type AnchorEvent = {
+  timestamp: string;
+  label: React.ReactNode;
+};
+
+const FORMAT = 'MMM d hh:mm:ss a';
+
+const renderKubeEvent = (event: KubeEvent) => {
+  return (
+    <Timeline.Item key={event.id}>
+      <Text size={11} c="gray.6" title={event.timestamp}>
+        {format(new Date(event.timestamp), FORMAT)}
+      </Text>
+      <Group spacing="xs" my={4}>
+        <Text size={12} color="white" fw="bold">
+          {event['object.reason']}
+        </Text>
+        {event['object.type'] && (
+          <Badge
+            size="xs"
+            fw="normal"
+            color={event['object.type'] === 'Normal' ? 'green' : 'yellow'}
+          >
+            {event['object.type']}
+          </Badge>
+        )}
+      </Group>
+      <Text size="xs">{event['object.note']}</Text>
+    </Timeline.Item>
+  );
+};
+
+export const KubeTimeline = ({
+  q,
+  anchorEvent,
+  dateRange,
+}: {
+  q: string;
+  dateRange?: [Date, Date];
+  anchorEvent?: AnchorEvent;
+}) => {
+  const startDate = React.useMemo(
+    () => dateRange?.[0] ?? sub(new Date(), { days: 7 }),
+    [dateRange],
+  );
+  const endDate = React.useMemo(
+    () => dateRange?.[1] ?? new Date(),
+    [dateRange],
+  );
+
+  const { data, isLoading } = api.useLogBatch({
+    q: `k8s.resource.name:"events" ${q}`,
+    limit: 50,
+    startDate,
+    endDate,
+    extraFields: [
+      'object.metadata.creationTimestamp',
+      'object.reason',
+      'object.note',
+      'object.type',
+      'type',
+    ],
+    order: 'desc',
+  });
+
+  const allPodEvents: KubeEvent[] = React.useMemo(
+    () =>
+      (data?.pages?.[0]?.data || []).sort(
+        (a, b) =>
+          new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime(),
+      ),
+    [data],
+  );
+
+  const podEventsBeforeAnchor = React.useMemo(() => {
+    return anchorEvent
+      ? allPodEvents.filter(event => {
+          return new Date(event.timestamp) < new Date(anchorEvent.timestamp);
+        })
+      : [];
+  }, [allPodEvents, anchorEvent]);
+
+  const podEventsAfterAnchor = React.useMemo(() => {
+    return anchorEvent
+      ? allPodEvents.filter(event => {
+          return new Date(event.timestamp) > new Date(anchorEvent.timestamp);
+        })
+      : [];
+  }, [allPodEvents, anchorEvent]);
+
+  // Scroll to anchor event if it exists
+  const anchorRef = React.useCallback(node => {
+    if (node !== null) {
+      // setting block to center causes the entire view to scroll
+      // todo - figure out how to scroll just the timeline and center the anchor event
+      node.scrollIntoView({ block: 'nearest' });
+    }
+  }, []);
+
+  if (isLoading) {
+    return (
+      <Text color="muted" ta="center">
+        Loading...
+      </Text>
+    );
+  }
+
+  if (allPodEvents.length === 0) {
+    return (
+      <Text color="muted" ta="center">
+        No events
+      </Text>
+    );
+  }
+
+  if (anchorEvent) {
+    return (
+      <Timeline bulletSize={12} lineWidth={1}>
+        {podEventsBeforeAnchor.map(renderKubeEvent)}
+        <Timeline.Item key={anchorEvent.timestamp} ref={anchorRef}>
+          <Text size={11} c="gray.6" title={anchorEvent.timestamp}>
+            {format(new Date(anchorEvent.timestamp), FORMAT)}
+          </Text>
+          <Group spacing="xs" my={4}>
+            <Text size={12} color="white" fw="bold">
+              {anchorEvent.label}
+            </Text>
+          </Group>
+        </Timeline.Item>
+        {podEventsAfterAnchor.map(renderKubeEvent)}
+      </Timeline>
+    );
+  } else {
+    return (
+      <Timeline bulletSize={12} lineWidth={1}>
+        {allPodEvents.map(renderKubeEvent)}
+      </Timeline>
+    );
+  }
+};
