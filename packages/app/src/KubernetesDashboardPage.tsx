39ba7689f8d43927490f37a9b289098fb5c62132
Shorpo <149748269+svc-shorpo@users.noreply.github.com>
2024-01-18 22:55:39 -0700

    feat: Node page (#252)
    ![Screenshot 2024-01-18 at 9 55 35 PM](https://github.com/hyperdxio/hyperdx/assets/149748269/f251106c-54eb-4099-8239-74221699bbe5)

--- packages/app/src/KubernetesDashboardPage.tsx
+++ packages/app/src/KubernetesDashboardPage.tsx
@@ -5,6 +5,7 @@ import cx from 'classnames';
 import { StringParam, useQueryParam, withDefault } from 'use-query-params';
 import {
   Anchor,
+  Badge,
   Card,
   Flex,
   Grid,
@@ -32,7 +33,7 @@ import HdxSearchInput from './SearchInput';
 import SearchTimeRangePicker from './SearchTimeRangePicker';
 import { parseTimeQuery, useTimeQuery } from './timeQuery';
 import { KubePhase } from './types';
-import { formatDistanceToNowStrictShort } from './utils';
+import { formatUptime } from './utils';
 import { formatNumber } from './utils';
 
 const SearchInput = React.memo(
@@ -227,7 +228,6 @@ const InfraPodsStatusTable = ({
         node: row.group[2],
         restarts: row['series_0.data'],
         uptime: row['series_1.data'],
-        startedAt: new Date(Date.now() + parseInt(row['series_1.data']) * 1000), // todo: find a way to format approx. duration without converting to date
         cpuAvg: row['series_2.data'],
         cpuLimit: row['series_3.data'],
         memAvg: row['series_4.data'],
@@ -367,14 +367,156 @@ const InfraPodsStatusTable = ({
                             </span>
                           </Tooltip>
                         </td>
-                        <td>
-                          {pod.startedAt
-                            ? formatDistanceToNowStrictShort(pod.startedAt)
-                            : '–'}
-                        </td>
+                        <td>{pod.uptime ? formatUptime(pod.uptime) : '–'}</td>
                         <td>{pod.restarts}</td>
                       </tr>
                     </Link>
+                  ))}
+                </tbody>
+              )}
+            </Table>
+          )}
+        </ScrollArea>
+      </Card.Section>
+    </Card>
+  );
+};
+
+const NodesTable = ({
+  where,
+  dateRange,
+}: {
+  where: string;
+  dateRange: [Date, Date];
+}) => {
+  const groupBy = ['k8s.node.name'];
+
+  const { data, isError, isLoading } = api.useMultiSeriesChart({
+    series: [
+      {
+        table: 'metrics',
+        field: 'k8s.node.cpu.utilization - Gauge',
+        type: 'table',
+        aggFn: 'avg',
+        where,
+        groupBy,
+      },
+      {
+        table: 'metrics',
+        field: 'k8s.node.memory.usage - Gauge',
+        type: 'table',
+        aggFn: 'avg',
+        where,
+        groupBy,
+      },
+      {
+        table: 'metrics',
+        field: 'k8s.node.condition_ready - Gauge',
+        type: 'table',
+        aggFn: 'avg',
+        where,
+        groupBy,
+      },
+      {
+        table: 'metrics',
+        field: 'k8s.node.uptime - Sum',
+        type: 'table',
+        aggFn: 'avg',
+        where,
+        groupBy,
+      },
+    ],
+    endDate: dateRange[1] ?? new Date(),
+    startDate: dateRange[0] ?? new Date(),
+    seriesReturnType: 'column',
+  });
+
+  const nodesList = React.useMemo(() => {
+    if (!data) {
+      return [];
+    }
+
+    return data.data.map((row: any) => {
+      return {
+        name: row.group[0],
+        namespace: row.group[1],
+        cpuAvg: row['series_0.data'],
+        memAvg: row['series_1.data'],
+        ready: row['series_2.data'],
+        uptime: row['series_3.data'],
+      };
+    });
+  }, [data]);
+
+  return (
+    <Card p="md">
+      <Card.Section p="md" py="xs" withBorder>
+        Nodes
+      </Card.Section>
+      <Card.Section>
+        <ScrollArea
+          viewportProps={{
+            style: { maxHeight: 300 },
+          }}
+        >
+          {isError ? (
+            <div className="p-4 text-center text-slate-500 fs-8">
+              Unable to load pod metrics
+            </div>
+          ) : !isLoading && nodesList.length === 0 ? (
+            <div className="p-4 text-center text-slate-500 fs-8">
+              No nodes found
+            </div>
+          ) : (
+            <Table horizontalSpacing="md" highlightOnHover>
+              <thead className="muted-thead">
+                <tr>
+                  <th>Node</th>
+                  <th style={{ width: 130 }}>Status</th>
+                  <th style={{ width: 130 }}>CPU</th>
+                  <th style={{ width: 130 }}>Memory</th>
+                  <th style={{ width: 130 }}>Uptime</th>
+                </tr>
+              </thead>
+              {isLoading ? (
+                <tbody>
+                  {Array.from({ length: 4 }).map((_, index) => (
+                    <tr key={index}>
+                      {Array.from({ length: 5 }).map((_, index) => (
+                        <td key={index}>
+                          <Skeleton height={8} my={6} />
+                        </td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              ) : (
+                <tbody>
+                  {nodesList.map(node => (
+                    <tr key={node.name}>
+                      <td>{node.name || 'N/A'}</td>
+                      <td>
+                        {node.ready === 1 ? (
+                          <Badge color="green" fw="normal" tt="none" size="md">
+                            Ready
+                          </Badge>
+                        ) : (
+                          <Badge color="red" fw="normal" tt="none" size="md">
+                            Not Ready
+                          </Badge>
+                        )}
+                      </td>
+                      <td>
+                        {formatNumber(
+                          node.cpuAvg,
+                          K8S_CPU_PERCENTAGE_NUMBER_FORMAT,
+                        )}
+                      </td>
+                      <td>
+                        {formatNumber(node.memAvg, K8S_MEM_NUMBER_FORMAT)}
+                      </td>
+                      <td>{node.uptime ? formatUptime(node.uptime) : '–'}</td>
+                    </tr>
                   ))}
                 </tbody>
               )}
@@ -562,7 +704,61 @@ export default function KubernetesDashboardPage() {
               </Grid.Col>
             </Grid>
           </Tabs.Panel>
-          <Tabs.Panel value="nodes">Nodes</Tabs.Panel>
+          <Tabs.Panel value="nodes">
+            <Grid>
+              <Grid.Col span={6}>
+                <Card p="md">
+                  <Card.Section p="md" py="xs" withBorder>
+                    CPU Usage
+                  </Card.Section>
+                  <Card.Section p="md" py="sm" h={CHART_HEIGHT}>
+                    <HDXLineChart
+                      config={{
+                        dateRange,
+                        granularity: convertDateRangeToGranularityString(
+                          dateRange,
+                          60,
+                        ),
+                        groupBy: 'k8s.node.name',
+                        where: whereClause,
+                        table: 'metrics',
+                        aggFn: 'avg',
+                        field: 'k8s.node.cpu.utilization - Gauge',
+                        numberFormat: K8S_CPU_PERCENTAGE_NUMBER_FORMAT,
+                      }}
+                    />
+                  </Card.Section>
+                </Card>
+              </Grid.Col>
+              <Grid.Col span={6}>
+                <Card p="md">
+                  <Card.Section p="md" py="xs" withBorder>
+                    Memory Usage
+                  </Card.Section>
+                  <Card.Section p="md" py="sm" h={CHART_HEIGHT}>
+                    <HDXLineChart
+                      config={{
+                        dateRange,
+                        granularity: convertDateRangeToGranularityString(
+                          dateRange,
+                          60,
+                        ),
+                        groupBy: 'k8s.node.name',
+                        where: whereClause,
+                        table: 'metrics',
+                        aggFn: 'avg',
+                        field: 'k8s.node.memory.usage - Gauge',
+                        numberFormat: K8S_MEM_NUMBER_FORMAT,
+                      }}
+                    />
+                  </Card.Section>
+                </Card>
+              </Grid.Col>
+              <Grid.Col span={12}>
+                <NodesTable dateRange={dateRange} where={whereClause} />
+              </Grid.Col>
+            </Grid>
+          </Tabs.Panel>
           <Tabs.Panel value="namespaces">Namespaces</Tabs.Panel>
           <Tabs.Panel value="clusters">Clusters</Tabs.Panel>
         </div>
