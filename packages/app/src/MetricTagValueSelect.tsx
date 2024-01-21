0494816d73ebe776be8c457eeceda7d90dc152b5
Mike Shi <mike@deploysentinel.com>
2024-01-20 20:32:41 -0800

    Add k8s resource filter dropdowns (#256)
    <img width="1275" alt="image" src="https://github.com/hyperdxio/hyperdx/assets/2781687/0206fbc7-2949-470c-b102-d0e081cda6ca">

---
+++ packages/app/src/MetricTagValueSelect.tsx
@@ -0,0 +1,64 @@
+import React, { useMemo } from 'react';
+import { Select } from '@mantine/core';
+
+import api from './api';
+
+export default function MetricTagValueSelect({
+  metricName,
+  metricAttribute,
+  value,
+  onChange,
+  dropdownOpenWidth,
+  dropdownClosedWidth,
+  ...selectProps
+}: {
+  metricName: string;
+  metricAttribute: string;
+  value: string;
+  dropdownOpenWidth?: number;
+  dropdownClosedWidth?: number;
+  onChange: (value: string) => void;
+} & Partial<React.ComponentProps<typeof Select>>) {
+  const { data: metricTagsData, isLoading: isMetricTagsLoading } =
+    api.useMetricsTags();
+
+  const options = useMemo(() => {
+    const tags =
+      metricTagsData?.data?.filter(metric => metric.name === metricName)?.[0]
+        ?.tags ?? [];
+
+    const valueSet = new Set<string>();
+
+    tags.forEach(tag => {
+      Object.entries(tag).forEach(([name, value]) => {
+        if (name === metricAttribute) {
+          valueSet.add(value);
+        }
+      });
+    });
+
+    return Array.from(valueSet);
+  }, [metricTagsData, metricName]);
+
+  const [dropdownOpen, setDropdownOpen] = React.useState(false);
+
+  return (
+    <Select
+      searchable
+      clearable
+      allowDeselect
+      maxDropdownHeight={280}
+      disabled={isMetricTagsLoading}
+      radius="md"
+      variant="filled"
+      value={value}
+      onChange={onChange}
+      w={dropdownOpen ? dropdownOpenWidth ?? 200 : dropdownClosedWidth ?? 200}
+      limit={20}
+      data={options}
+      onDropdownOpen={() => setDropdownOpen(true)}
+      onDropdownClose={() => setDropdownOpen(false)}
+      {...selectProps}
+    />
+  );
+}
