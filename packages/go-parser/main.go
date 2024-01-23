c532c9bb3cb91671d2497b29e65a5b280647d9fd
Warren <5959690+wrn14897@users.noreply.github.com>
2024-01-22 16:42:42 -0800

    feat: install hyperdx to go-parser in dev env (#272)

--- packages/go-parser/main.go
+++ packages/go-parser/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"bytes"
 	"compress/gzip"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"slices"
@@ -14,13 +16,21 @@ import (
 	"github.com/DataDog/go-sqllexer"
 	"github.com/gin-gonic/gin"
 	"github.com/hashicorp/go-retryablehttp"
+	"github.com/hyperdxio/opentelemetry-go/otelzap"
+	"github.com/hyperdxio/opentelemetry-logs-go/exporters/otlp/otlplogs"
+	sdk "github.com/hyperdxio/opentelemetry-logs-go/sdk/logs"
+	"github.com/hyperdxio/otel-config-go/otelconfig"
 	"github.com/xwb1989/sqlparser"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
 )
 
 var (
 	VERSION        = "0.0.1"
 	PORT           = os.Getenv("PORT")
 	AGGREGATOR_URL = os.Getenv("AGGREGATOR_API_URL")
+	SERVICE_NAME   = os.Getenv("OTEL_SERVICE_NAME")
 	// https://opentelemetry.io/docs/specs/semconv/database/database-spans/#:~:text=db.system%20has%20the%20following%20list%20of%20well%2Dknown%20values
 	NON_SQL_DB_SYSTEMS = []string{
 		"adabas",
@@ -69,7 +79,44 @@ func isSQLValid(sql string) (bool, error) {
 	return true, nil
 }
 
+// attach trace id to the log
+func WithTraceMetadata(ctx context.Context, logger *zap.Logger) *zap.Logger {
+	spanContext := trace.SpanContextFromContext(ctx)
+	if !spanContext.IsValid() {
+		// ctx does not contain a valid span.
+		// There is no trace metadata to add.
+		return logger
+	}
+	return logger.With(
+		zap.String("trace_id", spanContext.TraceID().String()),
+		zap.String("span_id", spanContext.SpanID().String()),
+	)
+}
+
 func main() {
+	// Initialize otel config and use it across the entire app
+	otelShutdown, err := otelconfig.ConfigureOpenTelemetry()
+	if err != nil {
+		log.Fatalf("error setting up OTel SDK - %e", err)
+	}
+
+	defer otelShutdown()
+
+	ctx := context.Background()
+
+	// configure opentelemetry logger provider
+	logExporter, _ := otlplogs.NewExporter(ctx)
+	loggerProvider := sdk.NewLoggerProvider(
+		sdk.WithBatcher(logExporter),
+	)
+
+	// gracefully shutdown logger to flush accumulated signals before program finish
+	defer loggerProvider.Shutdown(ctx)
+
+	// create new logger with opentelemetry zap core and set it globally
+	logger := zap.New(otelzap.NewOtelCore(loggerProvider))
+	zap.ReplaceGlobals(logger)
+
 	normalizer := sqllexer.NewNormalizer(
 		sqllexer.WithCollectComments(false),
 		sqllexer.WithCollectCommands(false),
@@ -77,6 +124,7 @@ func main() {
 		sqllexer.WithKeepSQLAlias(false),
 	)
 	router := gin.New()
+	router.Use(otelgin.Middleware(SERVICE_NAME))
 	router.Use(gin.Recovery()) // recover from panics
 	router.Use(gin.Logger())   // log requests to stdout
 	router.GET("/health", func(c *gin.Context) {
@@ -86,8 +134,10 @@ func main() {
 	})
 
 	router.POST("/", func(c *gin.Context) {
+		_logger := WithTraceMetadata(c.Request.Context(), logger)
 		contentEncodingHeader := c.GetHeader("Content-Encoding")
 		if contentEncodingHeader != "gzip" {
+			_logger.Error("Error: Content-Encoding must be gzip")
 			fmt.Println("Error: Content-Encoding must be gzip")
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Content-Encoding must be gzip"})
 			return
@@ -96,6 +146,7 @@ func main() {
 		var logs []map[string]interface{}
 
 		if err := c.ShouldBindWith(&logs, &GzipJSONBinding{}); err != nil {
+			_logger.Error("Error: ", zap.Error(err))
 			fmt.Println("Error:", err)
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
@@ -109,14 +160,16 @@ func main() {
 		// ****************** Parse Logs/Spans **********************
 		// **********************************************************
 		t1 := time.Now()
+		skippedLogs := 0
 		for _, log := range logs {
 			dbStatement := log["b"].(map[string]interface{})["db.statement"]
 			dbSystem := log["b"].(map[string]interface{})["db.system"]
 			if dbStatement != nil {
-        // defaults to dbStatement
+				// defaults to dbStatement
 				log["b"].(map[string]interface{})["db.normalized_statement"] = dbStatement
 				if dbSystem != nil && slices.Contains(NON_SQL_DB_SYSTEMS, dbSystem.(string)) {
-					fmt.Println("Skipping non-SQL DB system:", dbSystem.(string))
+					skippedLogs++
+					// fmt.Println("Skipping non-SQL DB system:", dbSystem.(string))
 					continue
 				}
 				normalized, _, err := normalizer.Normalize(dbStatement.(string))
@@ -129,6 +182,12 @@ func main() {
 				log["b"].(map[string]interface{})["db.normalized_statement"] = obfuscated
 			}
 		}
+		_logger.Info(
+			"Parsed logs/spans",
+			zap.Int("n", len(logs)),
+			zap.Int("skipped", skippedLogs),
+			zap.Int64("took", time.Since(t1).Milliseconds()),
+		)
 		fmt.Println("Parsing", len(logs), "logs/spans took:", time.Since(t1))
 
 		// **********************************************************
@@ -137,6 +196,7 @@ func main() {
 		t2 := time.Now()
 		jsonData, err := json.Marshal(logs)
 		if err != nil {
+			_logger.Error("Error marshaling JSON: ", zap.Error(err))
 			fmt.Println("Error marshaling JSON:", err)
 			return
 		}
@@ -146,6 +206,7 @@ func main() {
 
 		req, err := retryablehttp.NewRequest("POST", AGGREGATOR_URL, bytes.NewBuffer(jsonData))
 		if err != nil {
+			_logger.Error("Error creating request: ", zap.Error(err))
 			fmt.Println("Error creating request:", err)
 			return
 		}
@@ -154,17 +215,24 @@ func main() {
 
 		resp, err := retryClient.Do(req)
 		if err != nil {
+			_logger.Error("Error sending request: ", zap.Error(err))
 			fmt.Println("Error sending request:", err)
 			return
 		}
 		defer resp.Body.Close()
 
 		if resp.StatusCode != http.StatusOK {
+			_logger.Error("Unexpected response status: ", zap.String("status", resp.Status))
 			fmt.Println("Unexpected response status:", resp.Status)
 			return
 		}
 
-		fmt.Println("Sending", len(logs), "logs/spans took:", time.Since(t2))
+		_logger.Info(
+      "Sent logs/spans",
+      zap.Int("n", len(logs)),
+      zap.Int64("took", time.Since(t2).Milliseconds()),
+    )
+		fmt.Println("Sent", len(logs), "logs/spans took:", time.Since(t2))
 	})
 
 	if err := http.ListenAndServe(":"+PORT, router); err != nil {
