7dbb1d6f002b7503daa616b96102b58343f528fd
Mike Shi <mike@deploysentinel.com>
2024-01-25 00:53:38 -0800

    Add /charts/series ext api (#279)

---
+++ packages/api/src/routers/external-api/v1/charts.ts
@@ -0,0 +1,90 @@
+import opentelemetry, { SpanStatusCode } from '@opentelemetry/api';
+import express from 'express';
+import _ from 'lodash';
+import { z } from 'zod';
+import { validateRequest } from 'zod-express-middleware';
+
+import * as clickhouse from '@/clickhouse';
+import { getTeam } from '@/controllers/team';
+import { translateExternalSeriesToInternalSeries } from '@/utils/externalApi';
+import { externalQueryChartSeriesSchema } from '@/utils/zod';
+
+const router = express.Router();
+
+router.post(
+  '/series',
+  validateRequest({
+    body: z.object({
+      series: z
+        .array(externalQueryChartSeriesSchema)
+        .max(5)
+        .refine(
+          series => {
+            const groupByFields = series[0].groupBy;
+            return series.every(s => _.isEqual(s.groupBy, groupByFields));
+          },
+          {
+            message: 'All series must have the same groupBy fields',
+          },
+        ),
+      endTime: z.number(),
+      granularity: z.nativeEnum(clickhouse.Granularity).optional(),
+      startTime: z.number(),
+      seriesReturnType: z.optional(z.nativeEnum(clickhouse.SeriesReturnType)),
+    }),
+  }),
+  async (req, res, next) => {
+    try {
+      const teamId = req.user?.team;
+      const { endTime, granularity, startTime, seriesReturnType, series } =
+        req.body;
+
+      if (teamId == null) {
+        return res.sendStatus(403);
+      }
+
+      const team = await getTeam(teamId);
+      if (team == null) {
+        return res.sendStatus(403);
+      }
+
+      const internalSeries = series.map(s =>
+        translateExternalSeriesToInternalSeries({
+          type: 'time', // just to reuse the same fn
+          ...s,
+        }),
+      );
+
+      const MAX_NUM_GROUPS = 20;
+
+      const chResponse = await clickhouse.getMultiSeriesChart({
+        series: internalSeries,
+        endTime: endTime,
+        granularity,
+        maxNumGroups: MAX_NUM_GROUPS,
+        startTime: startTime,
+        tableVersion: team.logStreamTableVersion,
+        teamId: teamId.toString(),
+        seriesReturnType,
+      });
+
+      res.json({
+        data: chResponse.data.map(d => {
+          // @ts-ignore
+          const { rank, rank_order_by_value, ts_bucket, ...rowData } = d;
+          return {
+            ...rowData,
+            ts_bucket: ts_bucket * 1000,
+          };
+        }),
+      });
+    } catch (e) {
+      const span = opentelemetry.trace.getActiveSpan();
+      span?.recordException(e as Error);
+      span?.setStatus({ code: SpanStatusCode.ERROR });
+      next(e);
+    }
+  },
+);
+
+export default router;
