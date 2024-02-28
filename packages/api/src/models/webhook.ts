865b5de14d5e87e05b41867bc985da0825f2d480
Warren <5959690+wrn14897@users.noreply.github.com>
2024-02-27 23:45:53 -0800

    feat: alert template message pt3 (#326)

--- packages/api/src/models/webhook.ts
+++ packages/api/src/models/webhook.ts
@@ -1,25 +1,40 @@
 import { ObjectId } from 'mongodb';
 import mongoose, { Schema } from 'mongoose';
 
+export enum WebhookService {
+  Slack = 'slack',
+}
+
 export interface IWebhook {
   _id: ObjectId;
   createdAt: Date;
   name: string;
-  service: string;
+  service: WebhookService;
   team: ObjectId;
   updatedAt: Date;
   url: string;
 }
 
-export default mongoose.model<IWebhook>(
-  'Webhook',
-  new Schema<IWebhook>(
-    {
-      team: { type: Schema.Types.ObjectId, ref: 'Team' },
-      service: String,
-      name: String,
-      url: String,
+const WebhookSchema = new Schema<IWebhook>(
+  {
+    team: { type: Schema.Types.ObjectId, ref: 'Team' },
+    service: {
+      type: String,
+      enum: Object.values(WebhookService),
+      required: true,
     },
-    { timestamps: true },
-  ),
+    name: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true },
 );
+
+WebhookSchema.index({ team: 1, service: 1, name: 1 }, { unique: true });
+
+export default mongoose.model<IWebhook>('Webhook', WebhookSchema);
