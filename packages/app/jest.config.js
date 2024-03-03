7dbe8d519d78d414408135e02523a6b60663bd1d
Warren <5959690+wrn14897@users.noreply.github.com>
2024-03-03 14:03:05 -0800

    ci: uninstall @deploysentinel/jest-rtl-debugger + bump @testing-library/react (#328)

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
-  testEnvironment: '@deploysentinel/jest-rtl-debugger/environment',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testEnvironment: 'jsdom',
   globalSetup: '<rootDir>/global-setup.js',
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.(css|scss|sass)$':
-      '@deploysentinel/jest-rtl-debugger/transforms/css',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
