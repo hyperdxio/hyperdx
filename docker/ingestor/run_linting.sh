f3a1dffd79f3143d03ade9bdad3a98ee073ae10d
Warren <5959690+wrn14897@users.noreply.github.com>
2024-03-28 23:17:50 -0700

    fix: set ingestor HYPERDX_API_KEY default var

--- docker/ingestor/run_linting.sh
+++ docker/ingestor/run_linting.sh
@@ -4,6 +4,5 @@ directory="./docker/ingestor"
 
 export AGGREGATOR_API_URL="http://aggregator:8001"
 export GO_PARSER_API_URL="http://go-parser:7777"
-export HYPERDX_API_KEY="tacocat"
 
 vector validate --no-environment $directory/*.toml
