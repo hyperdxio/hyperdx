51f3db2e918008414450c108319180d58832e4c6
Mike Shi <mike@deploysentinel.com>
2024-03-28 17:46:30 -0700

    Minimal local mode (#148)
    Not ready for prime time yet, but does accomplish the basics of #7
    
    A few things to iron out:
    1. Clean up UI (remove log out, log patterns, etc.)
    2. Tag and push image to ghcr
    3. Document how to use this image
    4. Allow user customization of ports
    5. See if we can trim image size down a bit (it's ~2GB right now)
    6. See if we can trim down memory usage (it's a bit under 1GB right now), I suspect ~half is from CH.

---
+++ docker/local/entry.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+export HYPERDX_LOG_LEVEL="error"
+# https://clickhouse.com/docs/en/operations/server-configuration-parameters/settings#logger
+export CLICKHOUSE_LOG_LEVEL="error"
+
+# User-facing Services
+export AGGREGATOR_API_URL="http://localhost:8001"
+export INGESTOR_API_URL="http://localhost:8002"
+export SERVER_URL="http://localhost:8000"
+export FRONTEND_URL="http://localhost:8080"
+
+# Internal Services
+export CLICKHOUSE_HOST="http://localhost:8123"
+export MONGO_URI="mongodb://localhost:27017/hyperdx"
+export REDIS_URI="redis://localhost:6379"
+
+export EXPRESS_SESSION_SECRET="hyperdx is cool 👋"
+export IS_LOCAL_APP_MODE="DANGEROUSLY_is_local_app_mode💀"
+export NEXT_TELEMETRY_DISABLED="1"
+
+# Simulate Docker Service DNS
+echo "127.0.0.1      ingestor" >> /etc/hosts
+echo "127.0.0.1      aggregator" >> /etc/hosts
+
+# Start Clickhouse Server
+/entrypoint.sh &
+
+# Start Redis Server
+redis-server &
+
+# Start Mongo Server
+mongod --quiet --dbpath /data/db &
+
+# Start Vector Ingestor
+ENABLE_GO_PARSER="false" \
+GO_PARSER_API_URL="http://go-parser:7777" \
+ENABLE_TOKEN_MATCHING_CHECK="false" \
+vector \
+  -c /etc/vector/sources.toml \
+  -c /etc/vector/core.toml \
+  -c /etc/vector/http-sinks.toml \
+  --require-healthy true &
+
+# Start Otel Collector
+otelcol-contrib --config /etc/otelcol-contrib/config.yaml &
+
+# Aggregator
+APP_TYPE=aggregator \
+CLICKHOUSE_USER=aggregator \
+CLICKHOUSE_PASSWORD=aggregator \
+PORT=8001 \
+node /app/api/build/index.js &
+
+# Api
+APP_TYPE=api \
+CLICKHOUSE_USER=api \
+CLICKHOUSE_PASSWORD=api \
+PORT=8000 \
+node /app/api/build/index.js &
+
+# App
+NODE_ENV=production \
+PORT=8080 \
+node /app/app/server.js &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
