09ca863052667499849d480e0686408c8f27959e
Warren <5959690+wrn14897@users.noreply.github.com>
2024-04-03 11:12:32 -0700

    perf: decouple team member + invitation services (#364)
    The `/team` endpoint should only fetch team metadata instead of pulling both members and invites

--- packages/api/src/routers/api/team.ts
+++ packages/api/src/routers/api/team.ts
@@ -1,8 +1,9 @@
 import crypto from 'crypto';
 import express from 'express';
-import isemail from 'isemail';
 import pick from 'lodash/pick';
 import { serializeError } from 'serialize-error';
+import { z } from 'zod';
+import { validateRequest } from 'zod-express-middleware';
 
 import * as config from '@/config';
 import { getTags, getTeam, rotateTeamApiKey } from '@/controllers/team';
@@ -11,61 +12,6 @@ import TeamInvite from '@/models/teamInvite';
 import logger from '@/utils/logger';
 
 const router = express.Router();
-
-router.post('/', async (req, res, next) => {
-  try {
-    const { email: toEmail, name } = req.body;
-
-    if (!toEmail || !isemail.validate(toEmail)) {
-      return res.status(400).json({
-        message: 'Invalid email',
-      });
-    }
-
-    const teamId = req.user?.team;
-    const fromEmail = req.user?.email;
-
-    if (teamId == null) {
-      throw new Error(`User ${req.user?._id} not associated with a team`);
-    }
-
-    if (fromEmail == null) {
-      throw new Error(`User ${req.user?._id} doesnt have email`);
-    }
-
-    const team = await getTeam(teamId);
-    if (team == null) {
-      throw new Error(`Team ${teamId} not found`);
-    }
-
-    const toUser = await findUserByEmail(toEmail);
-    if (toUser) {
-      return res.status(400).json({
-        message: 'User already exists. Please contact HyperDX team for support',
-      });
-    }
-
-    let teamInvite = await TeamInvite.findOne({
-      teamId: team._id,
-      email: toEmail, // TODO: case insensitive ?
-    });
-
-    if (!teamInvite) {
-      teamInvite = await new TeamInvite({
-        teamId: team._id,
-        name,
-        email: toEmail, // TODO: case insensitive ?
-        token: crypto.randomBytes(32).toString('hex'),
-      }).save();
-    }
-
-    res.json({
-      url: `${config.FRONTEND_URL}/join-team?token=${teamInvite.token}`,
-    });
-  } catch (e) {
-    next(e);
-  }
-});
 
 const getSentryDSN = (apiKey: string, ingestorApiUrl: string) => {
   try {
@@ -93,37 +39,20 @@ router.get('/', async (req, res, next) => {
       throw new Error(`User has no id`);
     }
 
-    const team = await getTeam(teamId);
+    const team = await getTeam(teamId, [
+      '_id',
+      'allowedAuthMethods',
+      'apiKey',
+      'archive',
+      'name',
+      'slackAlert',
+    ]);
     if (team == null) {
       throw new Error(`Team ${teamId} not found for user ${userId}`);
     }
 
-    const teamUsers = await findUsersByTeam(teamId);
-    const teamInvites = await TeamInvite.find({});
-
     res.json({
-      ...pick(team.toJSON(), [
-        '_id',
-        'allowedAuthMethods',
-        'apiKey',
-        'archive',
-        'name',
-        'slackAlert',
-      ]),
-      users: teamUsers.map(user => ({
-        ...pick(user.toJSON({ virtuals: true }), [
-          'email',
-          'name',
-          'hasPasswordAuth',
-        ]),
-        isCurrentUser: user._id.equals(userId),
-      })),
-      teamInvites: teamInvites.map(ti => ({
-        createdAt: ti.createdAt,
-        email: ti.email,
-        name: ti.name,
-        url: `${config.FRONTEND_URL}/join-team?token=${ti.token}`,
-      })),
+      ...team.toJSON(),
       sentryDSN: getSentryDSN(team.apiKey, config.INGESTOR_API_URL),
     });
   } catch (e) {
@@ -144,13 +73,114 @@ router.patch('/apiKey', async (req, res, next) => {
   }
 });
 
+router.post(
+  '/invitation',
+  validateRequest({
+    body: z.object({
+      email: z.string().email(),
+      name: z.string().optional(),
+    }),
+  }),
+  async (req, res, next) => {
+    try {
+      const { email: toEmail, name } = req.body;
+      const teamId = req.user?.team;
+      const fromEmail = req.user?.email;
+
+      if (teamId == null) {
+        throw new Error(`User ${req.user?._id} not associated with a team`);
+      }
+
+      if (fromEmail == null) {
+        throw new Error(`User ${req.user?._id} doesnt have email`);
+      }
+
+      const toUser = await findUserByEmail(toEmail);
+      if (toUser) {
+        return res.status(400).json({
+          message:
+            'User already exists. Please contact HyperDX team for support',
+        });
+      }
+
+      let teamInvite = await TeamInvite.findOne({
+        teamId,
+        email: toEmail, // TODO: case insensitive ?
+      });
+
+      if (!teamInvite) {
+        teamInvite = await new TeamInvite({
+          teamId,
+          name,
+          email: toEmail, // TODO: case insensitive ?
+          token: crypto.randomBytes(32).toString('hex'),
+        }).save();
+      }
+
+      res.json({
+        url: `${config.FRONTEND_URL}/join-team?token=${teamInvite.token}`,
+      });
+    } catch (e) {
+      next(e);
+    }
+  },
+);
+
+router.get('/invitations', async (req, res, next) => {
+  try {
+    const teamId = req.user?.team;
+    if (teamId == null) {
+      throw new Error(`User ${req.user?._id} not associated with a team`);
+    }
+    const teamInvites = await TeamInvite.find(
+      { teamId },
+      {
+        createdAt: 1,
+        email: 1,
+        name: 1,
+        token: 1,
+      },
+    );
+    res.json({
+      data: teamInvites.map(ti => ({
+        createdAt: ti.createdAt,
+        email: ti.email,
+        name: ti.name,
+        url: `${config.FRONTEND_URL}/join-team?token=${ti.token}`,
+      })),
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get('/members', async (req, res, next) => {
+  try {
+    const teamId = req.user?.team;
+    if (teamId == null) {
+      throw new Error(`User ${req.user?._id} not associated with a team`);
+    }
+    const teamUsers = await findUsersByTeam(teamId);
+    res.json({
+      data: teamUsers.map(user => ({
+        ...pick(user.toJSON({ virtuals: true }), [
+          'email',
+          'name',
+          'hasPasswordAuth',
+        ]),
+      })),
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
 router.get('/tags', async (req, res, next) => {
   try {
     const teamId = req.user?.team;
     if (teamId == null) {
       throw new Error(`User ${req.user?._id} not associated with a team`);
     }
-
     const tags = await getTags(teamId);
     return res.json({ data: tags });
   } catch (e) {
