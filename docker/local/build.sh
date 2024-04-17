d1264d521c8d244d3a89020c50f4724401f18530
Warren <5959690+wrn14897@users.noreply.github.com>
2024-04-16 17:46:53 -0700

    fix: dns issue in local mode (#373)

--- docker/local/build.sh
+++ docker/local/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Meant to be run from the root of the repo
 
-docker build --squash -f ./docker/local/Dockerfile \
+docker build --squash -t hdx-oss-dev-local -f ./docker/local/Dockerfile \
   --build-context clickhouse=./docker/clickhouse \
   --build-context otel-collector=./docker/otel-collector \
   --build-context ingestor=./docker/ingestor \
