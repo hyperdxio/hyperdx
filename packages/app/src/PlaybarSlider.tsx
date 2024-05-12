6d99e3be9c4e43d3854d7a4c56c35c7f9e57f0e1
Ernest Iliiasov <ernestii@users.noreply.github.com>
2024-05-12 10:39:00 -0700

    fix: Update session player (#395)
    * Replace react-slider with mantine's slider + custom markers for better performance
    * Fix bug when opening a session replay with a ?ts param shows the first frame instead of correct frame at `ts`
    * abort loading events when navigating off the page
    
    before
    
    https://github.com/hyperdxio/hyperdx/assets/20255948/195ce791-2d31-4ae4-9700-0ff52f021171
    
    after
    
    https://github.com/hyperdxio/hyperdx/assets/20255948/8ec31ff4-c3c1-4c0d-9f04-29c123e9444f

---
+++ packages/app/src/PlaybarSlider.tsx
@@ -0,0 +1,98 @@
+import * as React from 'react';
+import { format } from 'date-fns';
+import { Slider, Tooltip } from '@mantine/core';
+
+import { truncateText } from './utils';
+
+import styles from '../styles/PlaybarSlider.module.scss';
+
+export type PlaybarMarker = {
+  id: string;
+  ts: number;
+  description: string;
+  isError: boolean;
+};
+
+type PlaybarSliderProps = {
+  value?: number;
+  min: number;
+  max: number;
+  markers?: PlaybarMarker[];
+  playerState: 'playing' | 'paused';
+  onChange: (value: number) => void;
+  setPlayerState: (playerState: 'playing' | 'paused') => void;
+};
+
+export const PlaybarSlider = ({
+  min,
+  max,
+  value,
+  markers,
+  playerState,
+  onChange,
+  setPlayerState,
+}: PlaybarSliderProps) => {
+  const valueLabelFormat = React.useCallback(
+    (ts: number) => {
+      const value = Math.max(ts - min, 0);
+      const minutes = Math.floor(value / 1000 / 60);
+      const seconds = Math.floor((value / 1000) % 60);
+      const timestamp = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+      const time = format(new Date(ts), 'hh:mm:ss a');
+      return `${timestamp} at ${time}`;
+    },
+    [min],
+  );
+
+  const markersContent = React.useMemo(
+    () =>
+      markers?.map(mark => (
+        <Tooltip
+          color={mark.isError ? 'red' : 'gray'}
+          key={mark.id}
+          label={truncateText(mark?.description ?? '', 240, '...', /\n/)}
+          position="top"
+          withArrow
+        >
+          <div
+            className={styles.markerDot}
+            style={{
+              backgroundColor: mark.isError
+                ? 'var(--mantine-color-red-6)'
+                : 'var(--mantine-color-gray-6)',
+              left: `${((mark.ts - min) / (max - min)) * 100}%`,
+            }}
+            onClick={() => onChange(mark.ts)}
+          />
+        </Tooltip>
+      )),
+    [markers, max, min, onChange],
+  );
+
+  const [prevPlayerState, setPrevPlayerState] = React.useState(playerState);
+  const handleMouseDown = React.useCallback(() => {
+    setPrevPlayerState(playerState);
+    setPlayerState('paused');
+  }, [playerState, setPlayerState]);
+  const handleMouseUp = React.useCallback(() => {
+    setPlayerState(prevPlayerState);
+  }, [prevPlayerState, setPlayerState]);
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.markers}>{markersContent}</div>
+      <Slider
+        color="gray.5"
+        size="sm"
+        min={min}
+        max={max}
+        value={value || min}
+        step={1000}
+        label={valueLabelFormat}
+        onChange={onChange}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+      />
+    </div>
+  );
+};
