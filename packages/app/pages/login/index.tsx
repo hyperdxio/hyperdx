288d5ccabdde4bd6090282be9e3541a7a2936412
Mike Shi <mike@hyperdx.io>
2024-11-29 00:57:23 -0800

    Use next-runtime-env for dynamic envs

--- packages/app/pages/login/index.tsx
+++ packages/app/pages/login/index.tsx
@@ -1,7 +1,5 @@
 import AuthPage from '@/AuthPage';
 
-export { getServerSideProps } from '@/emptyGetServerSideProps';
-
 export default function Login() {
   return (
     <div>
--- packages/app/src/config.ts
+++ packages/app/src/config.ts
@@ -1,6 +1,4 @@
-import getConfig from 'next/config';
-
-const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
+import { env } from 'next-runtime-env';
 
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL ?? 'http://localhost:8000'; // NEXT_PUBLIC_SERVER_URL can be empty string
@@ -11,10 +9,12 @@ export const CLICKHOUSE_HOST =
 
 // ONLY USED IN LOCAL MODE
 // ex: HDX_LOCAL_DEFAULT_CONNECTIONS='[{"id":"local","name":"Demo","host":"https://demo-ch.hyperdx.io","username":"demo","password":"demo"}]' HDX_LOCAL_DEFAULT_SOURCES='[{"id":"l701179602","kind":"trace","name":"Demo Traces","connection":"local","from":{"databaseName":"default","tableName":"otel_traces"},"timestampValueExpression":"Timestamp","defaultTableSelectExpression":"Timestamp, ServiceName, StatusCode, round(Duration / 1e6), SpanName","serviceNameExpression":"ServiceName","eventAttributesExpression":"SpanAttributes","resourceAttributesExpression":"ResourceAttributes","traceIdExpression":"TraceId","spanIdExpression":"SpanId","implicitColumnExpression":"SpanName","durationExpression":"Duration","durationPrecision":9,"parentSpanIdExpression":"ParentSpanId","spanKindExpression":"SpanKind","spanNameExpression":"SpanName","logSourceId":"l-758211293","statusCodeExpression":"StatusCode","statusMessageExpression":"StatusMessage"},{"id":"l-758211293","kind":"log","name":"Demo Logs","connection":"local","from":{"databaseName":"default","tableName":"otel_logs"},"timestampValueExpression":"TimestampTime","defaultTableSelectExpression":"Timestamp, ServiceName, SeverityText, Body","serviceNameExpression":"ServiceName","severityTextExpression":"SeverityText","eventAttributesExpression":"LogAttributes","resourceAttributesExpression":"ResourceAttributes","traceIdExpression":"TraceId","spanIdExpression":"SpanId","implicitColumnExpression":"Body","traceSourceId":"l701179602"}]' yarn dev:local
-export const HDX_LOCAL_DEFAULT_CONNECTIONS =
-  publicRuntimeConfig.hdxLocalDefaultConnections;
-export const HDX_LOCAL_DEFAULT_SOURCES =
-  publicRuntimeConfig.hdxLocalDefaultSources;
+export const HDX_LOCAL_DEFAULT_CONNECTIONS = env(
+  'NEXT_PUBLIC_HDX_LOCAL_DEFAULT_CONNECTIONS',
+);
+export const HDX_LOCAL_DEFAULT_SOURCES = env(
+  'NEXT_PUBLIC_HDX_LOCAL_DEFAULT_SOURCES',
+);
 
 export const HDX_API_KEY = process.env.HYPERDX_API_KEY as string; // for nextjs server
 export const HDX_SERVICE_NAME =
