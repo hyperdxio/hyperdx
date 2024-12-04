98bb2c51687d2f5d07c60f9e0bf2416b05933956
Warren <5959690+wrn14897@users.noreply.github.com>
2024-12-04 13:38:22 -0800

    style: multi-stage

--- docker/fullstack/entry.sh
+++ docker/fullstack/entry.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# User can specify either an entire SERVER_URL, or override slectively the 
-# Same applies to the frontend/app
 export SERVER_URL="${SERVER_URL:-${HYPERDX_APP_URL:-http://localhost}:${HYPERDX_API_PORT:-8000}}"
 export FRONTEND_URL="${FRONTEND_URL:-${HYPERDX_APP_URL:-http://localhost}:${HYPERDX_APP_PORT:-8080}}"
 
@@ -11,4 +9,3 @@ npx concurrently \
   "--names=API,APP" \
   "FRONTEND_URL=\"${FRONTEND_URL}\" APP_TYPE=api PORT=${HYPERDX_API_PORT:-8000} node /app/api/build/index.js" \
   "NEXT_PUBLIC_SERVER_URL=\"${SERVER_URL}\" /app/app/node_modules/.bin/next start -p ${HYPERDX_APP_PORT:-8080}"
-
