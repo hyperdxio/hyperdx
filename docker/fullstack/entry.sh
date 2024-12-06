737a499383c3345b2b40d1bc481447604ac1a36a
Warren <5959690+wrn14897@users.noreply.github.com>
2024-12-05 23:57:58 -0800

    fix: instrumentation entrypoint

--- docker/fullstack/entry.sh
+++ docker/fullstack/entry.sh
@@ -4,5 +4,5 @@
 npx concurrently \
   "--kill-others" \
   "--names=API,APP" \
-  "APP_TYPE=api PORT=${HYPERDX_API_PORT:-8000} node -r @hyperdx/node-opentelemetry/build/src/tracing /app/api/build/index.js" \
+  "APP_TYPE=api PORT=${HYPERDX_API_PORT:-8000} node -r /app/api/node_modules/@hyperdx/node-opentelemetry/build/src/tracing /app/api/build/index.js" \
   "/app/app/node_modules/.bin/next start -p ${HYPERDX_APP_PORT:-8080}"
