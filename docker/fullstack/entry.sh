b5e3207a3f25cad0820bd5942671ce31e07031f0
Warren <5959690+wrn14897@users.noreply.github.com>
2024-12-16 15:09:43 -0800

    style: specify version

--- docker/fullstack/entry.sh
+++ docker/fullstack/entry.sh
@@ -11,6 +11,3 @@ npx concurrently \
   "--names=API,APP" \
   "PORT=${HYPERDX_API_PORT:-8000} HYPERDX_APP_PORT=${HYPERDX_APP_PORT:-8080} node -r /app/api/node_modules/@hyperdx/node-opentelemetry/build/src/tracing /app/api/build/index.js" \
   "HYPERDX_API_PORT=${HYPERDX_API_PORT:-8000} /app/app/node_modules/.bin/next start -p ${HYPERDX_APP_PORT:-8080}"
-
-# Wait for any process to exit
-wait -n
