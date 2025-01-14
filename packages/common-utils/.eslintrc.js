942d055570b940ce5e4afadd2876c7f91386c428
Warren <5959690+wrn14897@users.noreply.github.com>
2025-01-14 10:28:47 -0800

    feat: introduce common-utils package

---
+++ packages/common-utils/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:n/recommended',
+    'plugin:prettier/recommended',
+    'plugin:security/recommended-legacy',
+  ],
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-empty-object-type': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-namespace': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'n/no-process-exit': 'warn',
+    'n/no-missing-import': 'off',
+    'n/no-unpublished-import': [
+      'error',
+      {
+        allowModules: ['supertest'],
+      },
+    ],
+    'n/no-unsupported-features/es-syntax': [
+      'error',
+      {
+        ignores: ['modules'],
+      },
+    ],
+    'prettier/prettier': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+};
