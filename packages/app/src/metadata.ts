86412d02efc45afbed9742ba62f2ef075f941e76
Mike Shi <mike@hyperdx.io>
2025-03-10 15:11:43 -0700

    revert metadata change

--- packages/app/src/metadata.ts
+++ packages/app/src/metadata.ts
@@ -2,7 +2,5 @@ import { getMetadata as _getMetadata } from '@hyperdx/common-utils/dist/metadata
 
 import { getClickhouseClient } from '@/clickhouse';
 
-const _metadata = _getMetadata(getClickhouseClient());
-
-// TODO: Get rid of this function and directly consume singleton
-export const getMetadata = () => _metadata;
+// TODO: Get rid of this function and convert to singleton
+export const getMetadata = () => _getMetadata(getClickhouseClient());
