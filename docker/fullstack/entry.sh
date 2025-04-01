e8ba5b432d1b32dbf2c78442e169ececbc7a6352
Warren <5959690+wrn14897@users.noreply.github.com>
2025-04-01 11:58:56 +0100

    feat: add check-alert task to fullstack build

--- docker/fullstack/entry.sh
+++ docker/fullstack/entry.sh
@@ -8,6 +8,7 @@ echo ""
 # Use concurrently to run both the API and App servers
 npx concurrently \
   "--kill-others" \
-  "--names=API,APP" \
+  "--names=API,APP,ALERT-TASK" \
   "PORT=${HYPERDX_API_PORT:-8000} HYPERDX_APP_PORT=${HYPERDX_APP_PORT:-8080} node -r /app/api/node_modules/@hyperdx/node-opentelemetry/build/src/tracing /app/api/build/index.js" \
-  "HYPERDX_API_PORT=${HYPERDX_API_PORT:-8000} /app/app/node_modules/.bin/next start -p ${HYPERDX_APP_PORT:-8080}"
+  "HYPERDX_API_PORT=${HYPERDX_API_PORT:-8000} /app/app/node_modules/.bin/next start -p ${HYPERDX_APP_PORT:-8080}" \
+  "node -r /app/api/node_modules/@hyperdx/node-opentelemetry/build/src/tracing /app/api/build/tasks/index.js check-alerts"
