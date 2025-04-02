1e51ed7de7cb5c5a29549c98703597f0afac6ce4
Warren <5959690+wrn14897@users.noreply.github.com>
2025-04-02 03:21:49 +0100

    feat: introduce RUN_SCHEDULED_TASKS_EXTERNALLY (#733)
    1. new env var RUN_SCHEDULED_TASKS_EXTERNALLY to opt out in-app task process
    2. introduce new `prod-extended` build that includes mongodb process
    3. GA k8s dashboard (only picking the connection)
    4. bake check-alert task into fullstack app build

---
+++ .changeset/twelve-ladybugs-confess.md
@@ -0,0 +1,5 @@
+---
+"@hyperdx/api": patch
+---
+
+feat: introduce RUN_SCHEDULED_TASKS_EXTERNALLY + enable in-app task
--- packages/api/src/config.ts
+++ packages/api/src/config.ts
@@ -26,6 +26,8 @@ export const OTEL_SERVICE_NAME = env.OTEL_SERVICE_NAME as string;
 export const PORT = Number.parseInt(env.PORT as string);
 export const REDIS_URL = env.REDIS_URL;
 export const USAGE_STATS_ENABLED = env.USAGE_STATS_ENABLED !== 'false';
+export const RUN_SCHEDULED_TASKS_EXTERNALLY =
+  env.RUN_SCHEDULED_TASKS_EXTERNALLY === 'true';
 
 // Only for single container local deployments, disable authentication
 export const IS_LOCAL_APP_MODE =
