5f7b2e6eabc5ea699415ab8327cd94b86f7b5b26
Mike Shi <mike@hyperdx.io>
2025-05-04 23:08:13 -0700

    chore: remove unused deps (redis, miner)

--- packages/api/src/utils/rateLimiter.ts
+++ packages/api/src/utils/rateLimiter.ts
@@ -1,14 +1,7 @@
 import rateLimit, { Options } from 'express-rate-limit';
-import RedisStore from 'rate-limit-redis';
-
-import redisClient from './redis';
 
 export default (config?: Partial<Options>) => async (req, rs, next) => {
   return rateLimit({
     ...config,
-    // Redis store configuration
-    store: new RedisStore({
-      sendCommand: (...args: string[]) => redisClient.sendCommand(args),
-    }),
   })(req, rs, next);
 };
