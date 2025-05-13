d34f15b8314187a64e9bac6ade370ea06c45c945
Aaron Knudtson <aaron.knudtson@outlook.com>
2025-05-13 12:51:50 -0400

    feat: add api key rotation

--- packages/app/src/TeamPage.tsx
+++ packages/app/src/TeamPage.tsx
@@ -1056,6 +1056,150 @@ function TeamNameSection() {
   );
 }
 
+const APIKeyCopyButton = ({
+  value,
+  dataTestId,
+}: {
+  value: string;
+  dataTestId?: string;
+}) => {
+  const [copied, setCopied] = useState(false);
+  return (
+    <CopyToClipboard text={value}>
+      <Button
+        onClick={() => setCopied(true)}
+        variant={copied ? 'light' : 'default'}
+        color="gray"
+        rightSection={
+          <div className="text-slate-300 ms-2 text-nowrap">
+            {copied ? (
+              <i className="bi bi-check-lg me-2" />
+            ) : (
+              <i className="bi bi-clipboard-fill me-2" />
+            )}
+            {copied ? 'Copied!' : 'Copy'}
+          </div>
+        }
+      >
+        <div data-test-id={dataTestId} className="text-wrap text-break">
+          {value}
+        </div>
+      </Button>
+    </CopyToClipboard>
+  );
+};
+
+function ApiKeySection() {
+  const { data: team, refetch: refetchTeam } = api.useTeam();
+  const { data: me, isLoading: isLoadingMe } = api.useMe();
+  const rotateTeamApiKey = api.useRotateTeamApiKey();
+  const hasAdminAccess =
+    me?.accountAccess === 'admin' || me?.accountAccess === 'readwrite';
+  const [
+    rotateApiKeyConfirmationModalShow,
+    setRotateApiKeyConfirmationModalShow,
+  ] = useState(false);
+  const rotateTeamApiKeyAction = () => {
+    rotateTeamApiKey.mutate(undefined, {
+      onSuccess: () => {
+        notifications.show({
+          color: 'green',
+          message: 'Revoked old API key and generated new key.',
+        });
+        refetchTeam();
+      },
+      onError: e => {
+        notifications.show({
+          color: 'red',
+          message: e.message,
+          autoClose: 5000,
+        });
+      },
+    });
+  };
+  const onConfirmUpdateTeamApiKey = () => {
+    rotateTeamApiKeyAction();
+    setRotateApiKeyConfirmationModalShow(false);
+  };
+
+  return (
+    <Box>
+      <Text size="md" c="gray.4">
+        API Keys
+      </Text>
+      <Divider my="md" />
+      <Card>
+        <Text c="gray.3" mb="md">
+          Ingestion API Key
+        </Text>
+        <Group gap="xs">
+          {team?.apiKey && (
+            <APIKeyCopyButton value={team.apiKey} dataTestId="api-key" />
+          )}
+          {hasAdminAccess && (
+            <Button
+              variant="light"
+              color="red"
+              onClick={() => setRotateApiKeyConfirmationModalShow(true)}
+            >
+              Rotate API Key
+            </Button>
+          )}
+        </Group>
+        <MModal
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          onClose={() => setRotateApiKeyConfirmationModalShow(false)}
+          opened={rotateApiKeyConfirmationModalShow}
+          size="lg"
+          title={
+            <Text size="xl">
+              <b>Rotate API Key</b>
+            </Text>
+          }
+        >
+          <MModal.Body>
+            <Text size="md">
+              Rotating the API key will invalidate your existing API key and
+              generate a new one for you. This action is <b>not reversible</b>.
+            </Text>
+            <Group justify="end">
+              <Button
+                variant="outline"
+                color="gray.5"
+                className="mt-2 px-4 ms-2 float-end"
+                size="sm"
+                onClick={() => setRotateApiKeyConfirmationModalShow(false)}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="outline"
+                color="red.6"
+                className="mt-2 px-4 float-end"
+                size="sm"
+                onClick={onConfirmUpdateTeamApiKey}
+              >
+                Confirm
+              </Button>
+            </Group>
+          </MModal.Body>
+        </MModal>
+      </Card>
+      {!isLoadingMe && me != null && (
+        <Card>
+          <Card.Section p="md">
+            <Text c="gray.3" mb="md">
+              Personal API Access Key
+            </Text>
+            <APIKeyCopyButton value={me.accessKey} dataTestId="api-key" />
+          </Card.Section>
+        </Card>
+      )}
+    </Box>
+  );
+}
+
 export default function TeamPage() {
   const { data: team, isLoading } = api.useTeam();
   const hasAllowedAuthMethods =
@@ -1082,7 +1226,7 @@ export default function TeamPage() {
               <ConnectionsSection />
               <IntegrationsSection />
               <TeamNameSection />
-              {/* <ApiKeySection /> */}
+              <ApiKeySection />
 
               {hasAllowedAuthMethods && (
                 <>
