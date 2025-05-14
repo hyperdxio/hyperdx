3394a122f413b9c6dfe98b21868685e6275f105e
Warren <5959690+wrn14897@users.noreply.github.com>
2025-05-14 14:58:32 -0700

    style: move tests

--- packages/common-utils/src/__tests__/utils.test.ts
+++ packages/common-utils/src/__tests__/utils.test.ts
@@ -1,6 +1,50 @@
-import { splitAndTrimCSV, splitAndTrimWithBracket } from '../utils';
+import { formatDate, splitAndTrimCSV, splitAndTrimWithBracket } from '../utils';
 
 describe('utils', () => {
+  describe('formatDate', () => {
+    it('12h utc', () => {
+      const date = new Date('2021-01-01T12:00:00Z');
+      expect(
+        formatDate(date, {
+          clock: '12h',
+          isUTC: true,
+        }),
+      ).toEqual('Jan 1 12:00:00 PM');
+    });
+
+    it('24h utc', () => {
+      const date = new Date('2021-01-01T12:00:00Z');
+      expect(
+        formatDate(date, {
+          clock: '24h',
+          isUTC: true,
+          format: 'withMs',
+        }),
+      ).toEqual('Jan 1 12:00:00.000');
+    });
+
+    it('12h local', () => {
+      const date = new Date('2021-01-01T12:00:00');
+      expect(
+        formatDate(date, {
+          clock: '12h',
+          isUTC: false,
+        }),
+      ).toEqual('Jan 1 12:00:00 PM');
+    });
+
+    it('24h local', () => {
+      const date = new Date('2021-01-01T12:00:00');
+      expect(
+        formatDate(date, {
+          clock: '24h',
+          isUTC: false,
+          format: 'withMs',
+        }),
+      ).toEqual('Jan 1 12:00:00.000');
+    });
+  });
+
   describe('splitAndTrimCSV', () => {
     it('should split a comma-separated string and trim whitespace', () => {
       expect(splitAndTrimCSV('a, b, c')).toEqual(['a', 'b', 'c']);
