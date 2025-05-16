2464456b444de21317edc00ceff4674869ad60af
Warren Lee <5959690+wrn14897@users.noreply.github.com>
2025-05-15 17:23:49 -0700

    style: deprecate old all-in-one dir

---
+++ docker/hyperdx/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Meant to be run from the root of the repo
+
+docker build --squash . -f ./docker/hyperdx/Dockerfile \
+    --build-context clickhouse=./docker/clickhouse \
+    --build-context otel-collector=./docker/otel-collector \
+    --build-context hyperdx=./docker/hyperdx \
+    --build-context api=./packages/api \
+    --build-context app=./packages/app \
+    --target all-in-one -t hyperdx/dev-all-in-one
