1894a840d46b2a4bef2663937e03dd48b661424f
Warren <5959690+wrn14897@users.noreply.github.com>
2025-05-22 00:41:04 -0700

    feat: init otel_logs + otel_traces tables

--- docker/clickhouse/local/init-db.sh
+++ docker/clickhouse/local/init-db.sh
@@ -1,7 +1,79 @@
 #!/bin/bash
 set -e
 
-clickhouse client -n <<-EOSQL
-  CREATE DATABASE docker;
-  CREATE TABLE docker.docker (x Int32) ENGINE = Log;
+clickhouse client -n <<'EOSQL'
+CREATE TABLE default.otel_logs
+(
+  `Timestamp` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
+  `TimestampTime` DateTime DEFAULT toDateTime(Timestamp),
+  `TraceId` String CODEC(ZSTD(1)),
+  `SpanId` String CODEC(ZSTD(1)),
+  `TraceFlags` UInt8,
+  `SeverityText` LowCardinality(String) CODEC(ZSTD(1)),
+  `SeverityNumber` UInt8,
+  `ServiceName` LowCardinality(String) CODEC(ZSTD(1)),
+  `Body` String CODEC(ZSTD(1)),
+  `ResourceSchemaUrl` LowCardinality(String) CODEC(ZSTD(1)),
+  `ResourceAttributes` Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+  `ScopeSchemaUrl` LowCardinality(String) CODEC(ZSTD(1)),
+  `ScopeName` String CODEC(ZSTD(1)),
+  `ScopeVersion` LowCardinality(String) CODEC(ZSTD(1)),
+  `ScopeAttributes` Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+  `LogAttributes` Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+  `__hdx_materialized_k8s.pod.name` String MATERIALIZED ResourceAttributes['k8s.pod.name'] CODEC(ZSTD(1)),
+  `__hdx_materialized_k8s.pod.uid` String MATERIALIZED ResourceAttributes['k8s.pod.uid'] CODEC(ZSTD(1)),
+  `__hdx_materialized_k8s.node.name` String MATERIALIZED ResourceAttributes['k8s.node.name'] CODEC(ZSTD(1)),
+  `__hdx_materialized_k8s.namespace.name` String MATERIALIZED ResourceAttributes['k8s.namespace.name'] CODEC(ZSTD(1)),
+  INDEX idx_trace_id TraceId TYPE bloom_filter(0.001) GRANULARITY 1,
+  INDEX idx_res_attr_key mapKeys(ResourceAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+  INDEX idx_res_attr_value mapValues(ResourceAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+  INDEX idx_scope_attr_key mapKeys(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+  INDEX idx_scope_attr_value mapValues(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+  INDEX idx_log_attr_key mapKeys(LogAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+  INDEX idx_log_attr_value mapValues(LogAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+  INDEX idx_lower_body lower(Body) TYPE tokenbf_v1(32768, 3, 0) GRANULARITY 8
+)
+ENGINE = MergeTree
+PARTITION BY toDate(TimestampTime)
+PRIMARY KEY (ServiceName, TimestampTime)
+ORDER BY (ServiceName, TimestampTime, Timestamp)
+TTL TimestampTime + toIntervalDay(3)
+SETTINGS index_granularity = 8192, ttl_only_drop_parts = 1;
+
+CREATE TABLE default.otel_traces
+(
+    `Timestamp` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
+    `TraceId` String CODEC(ZSTD(1)),
+    `SpanId` String CODEC(ZSTD(1)),
+    `ParentSpanId` String CODEC(ZSTD(1)),
+    `TraceState` String CODEC(ZSTD(1)),
+    `SpanName` LowCardinality(String) CODEC(ZSTD(1)),
+    `SpanKind` LowCardinality(String) CODEC(ZSTD(1)),
+    `ServiceName` LowCardinality(String) CODEC(ZSTD(1)),
+    `ResourceAttributes` Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+    `ScopeName` String CODEC(ZSTD(1)),
+    `ScopeVersion` String CODEC(ZSTD(1)),
+    `SpanAttributes` Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+    `Duration` UInt64 CODEC(ZSTD(1)),
+    `StatusCode` LowCardinality(String) CODEC(ZSTD(1)),
+    `StatusMessage` String CODEC(ZSTD(1)),
+    `Events.Timestamp` Array(DateTime64(9)) CODEC(ZSTD(1)),
+    `Events.Name` Array(LowCardinality(String)) CODEC(ZSTD(1)),
+    `Events.Attributes` Array(Map(LowCardinality(String), String)) CODEC(ZSTD(1)),
+    `Links.TraceId` Array(String) CODEC(ZSTD(1)),
+    `Links.SpanId` Array(String) CODEC(ZSTD(1)),
+    `Links.TraceState` Array(String) CODEC(ZSTD(1)),
+    `Links.Attributes` Array(Map(LowCardinality(String), String)) CODEC(ZSTD(1)),
+    INDEX idx_trace_id TraceId TYPE bloom_filter(0.001) GRANULARITY 1,
+    INDEX idx_res_attr_key mapKeys(ResourceAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+    INDEX idx_res_attr_value mapValues(ResourceAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+    INDEX idx_span_attr_key mapKeys(SpanAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+    INDEX idx_span_attr_value mapValues(SpanAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+    INDEX idx_duration Duration TYPE minmax GRANULARITY 1
+)
+ENGINE = MergeTree
+PARTITION BY toDate(Timestamp)
+ORDER BY (ServiceName, SpanName, toDateTime(Timestamp))
+TTL toDate(Timestamp) + toIntervalDay(3)
+SETTINGS index_granularity = 8192, ttl_only_drop_parts = 1;
 EOSQL
