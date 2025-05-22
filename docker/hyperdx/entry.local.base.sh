f42a1cdf917796ea2c8c4a50a8c4b6a674474e5b
Warren Lee <5959690+wrn14897@users.noreply.github.com>
2025-05-21 17:11:31 -0700

    fix: update existing endpoint values

--- docker/hyperdx/entry.local.base.sh
+++ docker/hyperdx/entry.local.base.sh
@@ -11,7 +11,7 @@ export SERVER_URL="http://127.0.0.1:${HYPERDX_API_PORT:-8000}"
 export FRONTEND_URL="${FRONTEND_URL:-${HYPERDX_APP_URL:-http://localhost}:${HYPERDX_APP_PORT:-8080}}"
 
 # Internal Services
-export CLICKHOUSE_SERVER_ENDPOINT="ch-server:9000"
+export CLICKHOUSE_SERVER_ENDPOINT="tcp://ch-server:9000"
 export MONGO_URI="mongodb://db:27017/hyperdx"
 
 export EXPRESS_SESSION_SECRET="hyperdx is cool 👋"
