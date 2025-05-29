615958f5142ab5e936a11b9d62cfff3c3bb58d1d
Aaron Knudtson <aaron.knudtson@outlook.com>
2025-05-29 00:12:05 -0400

    feat: use esbuild for building the api

---
+++ .changeset/tricky-laws-march.md
@@ -0,0 +1,5 @@
+---
+"@hyperdx/api": patch
+---
+
+api is now bundled in build and the Dockerfile benefits greatly
--- docker/hyperdx/entry.prod.sh
+++ docker/hyperdx/entry.prod.sh
@@ -15,6 +15,6 @@ echo ""
 npx concurrently \
   "--kill-others" \
   "--names=API,APP,ALERT-TASK" \
-  "PORT=${HYPERDX_API_PORT:-8000} HYPERDX_APP_PORT=${HYPERDX_APP_PORT:-8080} node -r /app/api/node_modules/@hyperdx/node-opentelemetry/build/src/tracing /app/api/packages/api/build/index.js" \
+  "PORT=${HYPERDX_API_PORT:-8000} HYPERDX_APP_PORT=${HYPERDX_APP_PORT:-8080} node -r /app/api/dist/tracing.bundle.js /app/api/dist/index.js" \
   "HYPERDX_API_PORT=${HYPERDX_API_PORT:-8000} /app/app/node_modules/.bin/next start -p ${HYPERDX_APP_PORT:-8080}" \
-  "node -r /app/api/node_modules/@hyperdx/node-opentelemetry/build/src/tracing /app/api/packages/api/build/tasks/index.js check-alerts"
+  "node -r /app/api/dist/tracing.bundle.js /app/api/dist/tasks/index.js check-alerts"
