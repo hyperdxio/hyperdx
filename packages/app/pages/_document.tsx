90ec1d7fdab5279441b528591c3819ea1f761514
Tom Alexander <teeohhem@gmail.com>
2025-06-09 22:18:27 -0400

    Upgrade to NextJS 14.x.x

--- packages/app/pages/_document.tsx
+++ packages/app/pages/_document.tsx
@@ -6,6 +6,20 @@ export default function Document() {
       <Head>
         <script src="/__ENV.js" />
         <script src="https://cdn.jsdelivr.net/pyodide/v0.27.2/full/pyodide.js"></script>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
