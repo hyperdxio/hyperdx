eb0d9e926b28c712c10d313b9cb8ce74b28ad9d9
Tom Alexander <teeohhem@gmail.com>
2025-06-13 16:05:44 -0400

    fix: Updatee ch connection form to use password field

--- packages/app/src/components/InputControlled.tsx
+++ packages/app/src/components/InputControlled.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { Control, Controller, FieldValues, Path } from 'react-hook-form';
-import { Input, InputProps } from '@mantine/core';
+import {
+  Input,
+  InputProps,
+  PasswordInput,
+  PasswordInputProps,
+} from '@mantine/core';
 
 interface InputControlledProps<T extends FieldValues>
   extends Omit<InputProps, 'name' | 'style'>,
+    Omit<React.InputHTMLAttributes<HTMLInputElement>, 'name' | 'size'> {
+  name: Path<T>;
+  control: Control<T>;
+  rules?: Parameters<Control<T>['register']>[1];
+}
+
+interface PasswordInputControlledProps<T extends FieldValues>
+  extends Omit<PasswordInputProps, 'name' | 'style'>,
     Omit<React.InputHTMLAttributes<HTMLInputElement>, 'name' | 'size'> {
   name: Path<T>;
   control: Control<T>;
@@ -23,6 +36,24 @@ export function InputControlled<T extends FieldValues>({
       rules={rules}
       render={({ field, fieldState: { error } }) => (
         <Input {...props} {...field} error={error?.message} />
+      )}
+    />
+  );
+}
+
+export function PasswordInputControlled<T extends FieldValues>({
+  name,
+  control,
+  rules,
+  ...props
+}: PasswordInputControlledProps<T>) {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={rules}
+      render={({ field, fieldState: { error } }) => (
+        <PasswordInput {...props} {...field} error={error?.message} />
       )}
     />
   );
