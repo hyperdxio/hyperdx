4235d1482499ae7941ca25af5bc3d152d618e59d
Drew Davis <drew.davis@clickhouse.com>
2025-09-08 13:41:47 -0400

    fix: Update Alerts when creating user is deleted

--- packages/api/src/controllers/user.ts
+++ packages/api/src/controllers/user.ts
@@ -1,6 +1,8 @@
-import type { ObjectId } from '@/models';
-import User from '@/models/user';
+import mongoose from 'mongoose';
 
+import type { ObjectId } from '@/models';
+import Alert from '@/models/alert';
+import User from '@/models/user';
 export function findUserByAccessKey(accessKey: string) {
   return User.findOne({ accessKey });
 }
@@ -24,9 +26,25 @@ export function findUsersByTeam(team: string | ObjectId) {
   return User.find({ team }).sort({ createdAt: 1 });
 }
 
-export function deleteTeamMember(teamId: string | ObjectId, userId: string) {
-  return User.findOneAndDelete({
-    team: teamId,
-    _id: userId,
-  });
+export async function deleteTeamMember(
+  teamId: string | ObjectId,
+  userIdToDelete: string,
+  userIdRequestingDelete: string | ObjectId,
+) {
+  const [, deletedUser] = await Promise.all([
+    Alert.updateMany(
+      { createdBy: new mongoose.Types.ObjectId(userIdToDelete), team: teamId },
+      {
+        $set: {
+          createdBy: new mongoose.Types.ObjectId(userIdRequestingDelete),
+        },
+      },
+    ),
+    User.findOneAndDelete({
+      team: teamId,
+      _id: userIdToDelete,
+    }),
+  ]);
+
+  return deletedUser;
 }
