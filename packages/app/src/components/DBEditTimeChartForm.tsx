da58956803bd9a2be06dc5597196da1a99a92d3c
Mike Shi <mike@hyperdx.io>
2025-09-16 17:49:30 -0700

    feat: Allow chart series to be reodered

--- packages/app/src/components/DBEditTimeChartForm.tsx
+++ packages/app/src/components/DBEditTimeChartForm.tsx
@@ -122,12 +122,14 @@ function ChartSeriesEditorComponent({
   index,
   namePrefix,
   onRemoveSeries,
+  onSwapSeries,
   onSubmit,
   setValue,
   showGroupBy,
   tableName: _tableName,
   watch,
   parentRef,
+  length,
 }: {
   control: Control<any>;
   databaseName: string;
@@ -137,11 +139,13 @@ function ChartSeriesEditorComponent({
   namePrefix: string;
   parentRef?: HTMLElement | null;
   onRemoveSeries: (index: number) => void;
+  onSwapSeries: (from: number, to: number) => void;
   onSubmit: () => void;
   setValue: UseFormSetValue<any>;
   showGroupBy: boolean;
   tableName: string;
   watch: UseFormWatch<any>;
+  length: number;
 }) {
   const aggFn = watch(`${namePrefix}aggFn`);
   const aggConditionLanguage = watch(
@@ -185,6 +189,28 @@ function ChartSeriesEditorComponent({
               />
             </div>
             {(index ?? -1) > 0 && (
+              <Button
+                variant="subtle"
+                color="gray"
+                size="xxs"
+                onClick={() => onSwapSeries(index, index - 1)}
+                title="Move up"
+              >
+                <i className="bi bi-arrow-up" />
+              </Button>
+            )}
+            {(index ?? -1) < length - 1 && (
+              <Button
+                variant="subtle"
+                color="gray"
+                size="xxs"
+                onClick={() => onSwapSeries(index, index + 1)}
+                title="Move down"
+              >
+                <i className="bi bi-arrow-down" />
+              </Button>
+            )}
+            {((index ?? -1) > 0 || length > 1) && (
               <Button
                 variant="subtle"
                 color="gray"
@@ -365,6 +391,7 @@ export default function EditTimeChartForm({
     fields,
     append,
     remove: removeSeries,
+    swap: swapSeries,
   } = useFieldArray({
     control: control as Control<SavedChartConfigWithSelectArray>,
     name: 'select',
@@ -637,6 +664,8 @@ export default function EditTimeChartForm({
                   parentRef={parentRef}
                   namePrefix={`select.${index}.`}
                   onRemoveSeries={removeSeries}
+                  length={fields.length}
+                  onSwapSeries={swapSeries}
                   onSubmit={onSubmit}
                   setValue={setValue}
                   connectionId={tableSource?.connection}
