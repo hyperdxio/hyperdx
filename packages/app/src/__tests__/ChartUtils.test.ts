8213d69b8e80b210827193b216a607e85166ed39
Drew Davis <drew.davis@clickhouse.com>
2026-01-06 11:02:02 -0500

    fix: Ensure displayed configs match queried configs in chart explorer (#1527)
    Closes HDX-3089
    Closes HDX-3083
    
    # Summary
    
    This PR ensures that the Generated SQL and Materialized View Explanations are generated for the same config that is queried. 
    
    Previously, this was not the case because child components (eg. DBTableChart, DBTimeChart, DBNumberChart) would transform the config passed down by DBEditTimeChart or Dashboard.Tile. Now, the same transformations are applied at the parent component.
    
    ## Demo
    
    <details>
    <summary>Table Tile MV Indicator now correctly ignores granularity</summary>
    
    Note that the time chart tiles cannot use MVs because the granularity is too small, but the table tile can use the MV:
    
    <img width="1495" height="884" alt="Screenshot 2025-12-29 at 9 00 16 AM" src="https://github.com/user-attachments/assets/324d3511-6b86-4bd8-87b0-824b4586bb9b" />
    </details>
    
    <details>
    <summary>Number and Table Chart generated SQL is correct</summary>
    
    Number Chart has no granularity, group, or order by:
    
    <img width="1493" height="830" alt="Screenshot 2025-12-29 at 9 14 08 AM" src="https://github.com/user-attachments/assets/34202de1-1d55-4675-9883-e15418f2bb2f" />
    
    Table Chart has implicit group by, limit, and order by applied:
    
    <img width="1495" height="837" alt="Screenshot 2025-12-29 at 9 14 00 AM" src="https://github.com/user-attachments/assets/2aa17106-527a-4e34-a76a-a6c1767a1e86" />
    
    </details>

--- packages/app/src/__tests__/ChartUtils.test.ts
+++ packages/app/src/__tests__/ChartUtils.test.ts
@@ -1,6 +1,21 @@
-import { SourceKind, TSource } from '@hyperdx/common-utils/dist/types';
+import {
+  ChartConfigWithDateRange,
+  SourceKind,
+  TSource,
+} from '@hyperdx/common-utils/dist/types';
 
-import { formatResponseForTimeChart } from '@/ChartUtils';
+import {
+  convertToNumberChartConfig,
+  convertToTableChartConfig,
+  convertToTimeChartConfig,
+  formatResponseForTimeChart,
+} from '@/ChartUtils';
+
+if (!globalThis.structuredClone) {
+  globalThis.structuredClone = (obj: any) => {
+    return JSON.parse(JSON.stringify(obj));
+  };
+}
 
 describe('ChartUtils', () => {
   describe('formatResponseForTimeChart', () => {
@@ -527,6 +542,114 @@ describe('ChartUtils', () => {
           isDashed: true,
         },
       ]);
+    });
+  });
+
+  describe('convertToTimeChartConfig', () => {
+    it('should set granularity when granularity is auto', () => {
+      const config = {
+        granularity: 'auto',
+        dateRange: [
+          new Date('2025-11-26T00:00:00Z'),
+          new Date('2025-11-27T00:00:00Z'),
+        ],
+      } as ChartConfigWithDateRange;
+
+      const granularityFromFunction =
+        convertToTimeChartConfig(config).granularity;
+
+      expect(granularityFromFunction).toBe('30 minute');
+    });
+
+    it('should set granularity when granularity is undefined', () => {
+      const config = {
+        dateRange: [
+          new Date('2025-11-26T00:00:00Z'),
+          new Date('2025-11-27T00:00:00Z'),
+        ],
+      } as ChartConfigWithDateRange;
+
+      const granularityFromFunction =
+        convertToTimeChartConfig(config).granularity;
+
+      expect(granularityFromFunction).toBe('30 minute');
+    });
+
+    it('should retain the specified granularity when not auto', () => {
+      const config = {
+        granularity: '5 minute',
+        dateRange: [
+          new Date('2025-11-26T00:00:00Z'),
+          new Date('2025-11-27T00:00:00Z'),
+        ],
+      } as ChartConfigWithDateRange;
+
+      const granularityFromFunction =
+        convertToTimeChartConfig(config).granularity;
+
+      expect(granularityFromFunction).toBe('5 minute');
+    });
+  });
+
+  describe('convertToNumberChartConfig', () => {
+    it('should remove granularity and groupBy from the config', () => {
+      const config = {
+        granularity: '5 minute',
+        groupBy: 'ServiceName',
+        dateRange: [
+          new Date('2025-11-26T00:00:00Z'),
+          new Date('2025-11-27T00:00:00Z'),
+        ],
+      } as ChartConfigWithDateRange;
+
+      const convertedConfig = convertToNumberChartConfig(config);
+
+      expect(convertedConfig.granularity).toBeUndefined();
+      expect(convertedConfig.groupBy).toBeUndefined();
+    });
+  });
+
+  describe('convertToTableChartConfig', () => {
+    it('should remove granularity from the config', () => {
+      const config = {
+        granularity: '5 minute',
+        dateRange: [
+          new Date('2025-11-26T00:00:00Z'),
+          new Date('2025-11-27T00:00:00Z'),
+        ],
+      } as ChartConfigWithDateRange;
+
+      const convertedConfig = convertToTableChartConfig(config);
+
+      expect(convertedConfig.granularity).toBeUndefined();
+    });
+
+    it('should apply a default sort if none is provided', () => {
+      const config = {
+        groupBy: 'ServiceName',
+        dateRange: [
+          new Date('2025-11-26T00:00:00Z'),
+          new Date('2025-11-27T00:00:00Z'),
+        ],
+      } as ChartConfigWithDateRange;
+
+      const convertedConfig = convertToTableChartConfig(config);
+
+      expect(convertedConfig.orderBy).toEqual('ServiceName');
+    });
+
+    it('should apply a default limit if none is provided', () => {
+      const config = {
+        groupBy: 'ServiceName',
+        dateRange: [
+          new Date('2025-11-26T00:00:00Z'),
+          new Date('2025-11-27T00:00:00Z'),
+        ],
+      } as ChartConfigWithDateRange;
+
+      const convertedConfig = convertToTableChartConfig(config);
+
+      expect(convertedConfig.limit).toEqual({ limit: 200 });
     });
   });
 });
