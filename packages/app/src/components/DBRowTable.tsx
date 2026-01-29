22f882d612e11710e40894aa887819d20ef16c99
Himanshu Kapoor <himanshu.kapoor@clickhouse.com>
2026-01-29 19:19:27 +0100

    fix: Do not trigger table search input on modals/drawers (#1675)
    You can use Cmd+F to search through the log search table. We've implemented a custom input that overrides the native input that allows you to search through virtual items. However, if you click on one of the rows, a drawer opens, and we don't want that search input to listen for keyboard events anymore at this point. This PR fixes this behaviour.
    
    https://github.com/user-attachments/assets/cbcaea4d-77a8-4c37-a368-bffc44ba4d59
    
    
    
    Fixes HDX-3302

--- packages/app/src/components/DBRowTable.tsx
+++ packages/app/src/components/DBRowTable.tsx
@@ -838,6 +838,7 @@ export const RawLogTable = memo(
             onNextMatch={tableSearch.handleNextMatch}
             isVisible={tableSearch.isSearchVisible}
             onVisibilityChange={tableSearch.setIsSearchVisible}
+            containerRef={tableContainerRef}
           />
           <div
             data-testid="search-results-table"
