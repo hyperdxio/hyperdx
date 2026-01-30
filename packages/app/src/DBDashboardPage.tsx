4c287b163ca1fc973132965cd815bc5a547ba231
Drew Davis <drew.davis@clickhouse.com>
2026-01-30 13:15:55 -0500

    fix: Fix External Dashboard APIs (#1681)
    Closes HDX-3303
    
    # Summary
    
    This PR reworks the external dashboard APIs in the following ways
    
    1. Previously, there was no way to create a renderable dashboard using the create or update routes, because the APIs saved dashboard data in a format incompatible with v2 of HyperDX. This PR introduces a transformation layer to transform external API request and response formats into the v2 dashboard format prior to saving.
    2. This PR also corrects and improves the openapi documentation associated with the dashboard endpoints.
    
    This PR introduces a number of **breaking changes** in the dashboard API, which is acceptable because the APIs are already (a) effectively non-functional for writing dashboard data and (b) not aligned at all with the previously published spec when reading dashboard data.
    
    1. The `dataSource` field has been removed from each series and replaced with a `sourceId` field, which specifies the sourceId of the source which should be queried. The `dataSource` argument was inflexible and could only specify `events`, `rrweb`, or `metrics` - these are leftover from V1, and are not very compatible with the design of HyperDX V2.
    2. Dashboard responses previously would have contained `config` fields, in the format of a V2 dashboard config. Results are now transformed and align with the spec, which contains config data in a `series` array.
    3. Additional validation has been added to prevent creation of some invalid dashboards

--- packages/app/src/DBDashboardPage.tsx
+++ packages/app/src/DBDashboardPage.tsx
@@ -395,12 +395,15 @@ const Tile = forwardRef(
                 config={queriedConfig}
               />
             )}
-            {queriedConfig?.displayType === DisplayType.Markdown && (
+            {/* Markdown charts may not have queriedConfig, if source is not set */}
+            {(queriedConfig?.displayType === DisplayType.Markdown ||
+              (!queriedConfig &&
+                chart.config.displayType === DisplayType.Markdown)) && (
               <HDXMarkdownChart
                 key={`${keyPrefix}-${chart.id}`}
                 title={title}
                 toolbarItems={toolbar}
-                config={queriedConfig}
+                config={queriedConfig ?? chart.config}
               />
             )}
             {queriedConfig?.displayType === DisplayType.Search && (
