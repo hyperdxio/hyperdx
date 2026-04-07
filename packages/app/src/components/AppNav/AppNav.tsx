f8d2edde5a3c30232716f83833650695a64abf98
Drew Davis <drew.davis@clickhouse.com>
2026-04-07 10:31:44 -0400

    feat: Show created/updated metadata for saved searches and dashboards (#2031)
    ## Summary
    
    This PR adds createdAt/By and updatedAt/By metadata to dashboard and saved searches.
    
    ### Screenshots or video
    
    <img width="1466" height="342" alt="Screenshot 2026-04-01 at 3 19 07 PM" src="https://github.com/user-attachments/assets/c349a3d5-f8e3-4155-9938-c8f005cdcd52" />
    <img width="1216" height="433" alt="Screenshot 2026-04-01 at 3 19 57 PM" src="https://github.com/user-attachments/assets/9542a631-bdda-484c-9cef-6b780667d1dc" />
    <img width="1196" height="345" alt="Screenshot 2026-04-01 at 3 19 46 PM" src="https://github.com/user-attachments/assets/c05cd0cc-2ca4-4397-8acb-e31a81b882ec" />
    <img width="1409" height="433" alt="Screenshot 2026-04-01 at 3 19 38 PM" src="https://github.com/user-attachments/assets/593a96d7-86be-45b2-9f0a-b3a8f00d1353" />
    <img width="1447" height="181" alt="Screenshot 2026-04-01 at 3 20 59 PM" src="https://github.com/user-attachments/assets/88742578-3dbd-4305-921f-e2ecdd11d5d4" />
    
    
    ### How to test locally or on Vercel
    
    This should be tested locally. In the preview environment, these fields are not populated (since they're maintained through automatic MongoDB createdAt/updatedAt values and createdBy/updatedBy values pulled from User accounts.
    
    ### References
    
    
    
    - Linear Issue: Closes HDX-3461
    - Related PRs:

--- packages/app/src/components/AppNav/AppNav.tsx
+++ packages/app/src/components/AppNav/AppNav.tsx
@@ -5,6 +5,10 @@ import cx from 'classnames';
 import HyperDX from '@hyperdx/browser';
 import { isBuilderSavedChartConfig } from '@hyperdx/common-utils/dist/guards';
 import {
+  AlertState,
+  SavedSearchListApiResponse,
+} from '@hyperdx/common-utils/dist/types';
+import {
   ActionIcon,
   Anchor,
   Badge,
@@ -36,7 +40,6 @@ import InstallInstructionModal from '@/InstallInstructionsModal';
 import OnboardingChecklist from '@/OnboardingChecklist';
 import { useSavedSearches } from '@/savedSearch';
 import { useLogomark, useWordmark } from '@/theme/ThemeProvider';
-import type { SavedSearch } from '@/types';
 import { UserPreferencesModal } from '@/UserPreferencesModal';
 import { useUserPreferences } from '@/useUserPreferences';
 import { useWindowSize } from '@/utils';
@@ -204,7 +207,7 @@ export default function AppNav({ fixed = false }: { fixed?: boolean }) {
   }, [meData]);
 
   const renderSavedSearchLink = useCallback(
-    (savedSearch: SavedSearch) => (
+    (savedSearch: SavedSearchListApiResponse) => (
       <Link
         href={`/search/${savedSearch.id}`}
         key={savedSearch.id}
