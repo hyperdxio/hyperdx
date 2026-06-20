ea3249a9f814bf6f6ef52b3d90aca12395268173
aryainguz <aryaninguz369@gmail.com>
2026-06-20 17:54:19 +0530

    fix: enforce password complexity on team invite acceptance

--- packages/api/src/utils/__tests__/validators.test.ts
+++ packages/api/src/utils/__tests__/validators.test.ts
@@ -3,15 +3,21 @@ import * as validators from '../validators';
 describe('validators', () => {
   describe('validatePassword', () => {
     it('should return true if password is valid', () => {
-      expect(validators.validatePassword('abcdefgh')).toBe(true);
+      expect(validators.validatePassword('aB3!efghijkl')).toBe(true);
+      expect(validators.validatePassword('ValidPass123!')).toBe(true);
     });
 
     it('should return false if password is invalid', () => {
-      expect(validators.validatePassword(null!)).toBe(false);
-      expect(validators.validatePassword(undefined!)).toBe(false);
+      expect(validators.validatePassword(null as any)).toBe(false);
+      expect(validators.validatePassword(undefined as any)).toBe(false);
       expect(validators.validatePassword('')).toBe(false);
       expect(validators.validatePassword('1234567')).toBe(false);
-      expect(validators.validatePassword('a'.repeat(65))).toBe(false);
+      expect(validators.validatePassword('abcdefghijk')).toBe(false); // 11 chars
+      expect(validators.validatePassword('abcdefghijkl')).toBe(false); // no upper/num/special
+      expect(validators.validatePassword('ABCDEFGHIJKL')).toBe(false); // no lower/num/special
+      expect(validators.validatePassword('Abcdefghijkl')).toBe(false); // no num/special
+      expect(validators.validatePassword('Abcdefghijk1')).toBe(false); // no special
+      expect(validators.validatePassword('Abcdefghijk!')).toBe(false); // no num
     });
   });
 });
