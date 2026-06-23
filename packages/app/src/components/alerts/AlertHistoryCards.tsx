1db64277d926a744724baace5352cb4a908a9e38
mrkaye97 <mrkaye97@gmail.com>
2026-06-23 14:18:59 -0400

    fix: show pending state in alert history as orange

--- packages/app/src/components/alerts/AlertHistoryCards.tsx
+++ packages/app/src/components/alerts/AlertHistoryCards.tsx
@@ -26,6 +26,17 @@ import styles from '../../../styles/AlertsPage.module.scss';
 
 const HISTORY_ITEMS = 18;
 
+function stateToBgColorClass(state: AlertState) {
+  switch (state) {
+    case AlertState.OK:
+      return styles.ok;
+    case AlertState.PENDING:
+      return styles.pending;
+    default:
+      return styles.alarm;
+  }
+}
+
 function AlertHistoryCard({
   history,
   alertUrl,
@@ -58,7 +69,7 @@ function AlertHistoryCard({
     <div
       className={cx(
         styles.historyCard,
-        history.state === AlertState.OK ? styles.ok : styles.alarm,
+        stateToBgColorClass(history.state),
         href && styles.clickable,
       )}
     />
