8fa5090323e5c4a207959c16cb7236c509c9332d
aryainguz <aryaninguz369@gmail.com>
2026-06-27 17:30:01 +0530

    fix(alerts): revert slack webhook retries due to idempotency issues

--- packages/api/src/utils/slack.ts
+++ packages/api/src/utils/slack.ts
@@ -1,16 +1,12 @@
 import { IncomingWebhook, IncomingWebhookSendArguments } from '@slack/webhook';
 
-import { withRetry } from './retry';
-
 export function postMessageToWebhook(
   webhookUrl: string,
   message: IncomingWebhookSendArguments,
 ) {
   const webhook = new IncomingWebhook(webhookUrl);
-  return withRetry(() =>
-    webhook.send({
-      text: message.text,
-      blocks: message.blocks,
-    }),
-  );
+  return webhook.send({
+    text: message.text,
+    blocks: message.blocks,
+  });
 }
