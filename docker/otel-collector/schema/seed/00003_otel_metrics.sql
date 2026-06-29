093544bdcad2e2f70c96f1b97d7340ae1795b2af
Aaron Knudtson <87577305+knudtty@users.noreply.github.com>
2026-06-29 15:26:53 -0400

    fix: change metrics PK for better memory usage

--- docker/otel-collector/schema/seed/00003_otel_metrics.sql
+++ docker/otel-collector/schema/seed/00003_otel_metrics.sql
@@ -9,16 +9,16 @@ CREATE TABLE IF NOT EXISTS ${DATABASE}.otel_metrics_gauge
     `ScopeDroppedAttrCount` UInt32 CODEC(ZSTD(1)),
     `ScopeSchemaUrl` String CODEC(ZSTD(1)),
     `ServiceName` LowCardinality(String) CODEC(ZSTD(1)),
-    `MetricName` String CODEC(ZSTD(1)),
+    `MetricName` LowCardinality(String) CODEC(ZSTD(1)),
     `MetricDescription` String CODEC(ZSTD(1)),
     `MetricUnit` String CODEC(ZSTD(1)),
     `Attributes` Map(LowCardinality(String), String) CODEC(ZSTD(1)),
-    `StartTimeUnix` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
-    `TimeUnix` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
+    `StartTimeUnix` DateTime CODEC(Delta(8), ZSTD(1)),
+    `TimeUnix` DateTime CODEC(Delta(8), ZSTD(1)),
     `Value` Float64 CODEC(ZSTD(1)),
     `Flags` UInt32 CODEC(ZSTD(1)),
     `Exemplars.FilteredAttributes` Array(Map(LowCardinality(String), String)) CODEC(ZSTD(1)),
-    `Exemplars.TimeUnix` Array(DateTime64(9)) CODEC(ZSTD(1)),
+    `Exemplars.TimeUnix` Array(DateTime) CODEC(ZSTD(1)),
     `Exemplars.Value` Array(Float64) CODEC(ZSTD(1)),
     `Exemplars.SpanId` Array(String) CODEC(ZSTD(1)),
     `Exemplars.TraceId` Array(String) CODEC(ZSTD(1)),
@@ -27,11 +27,12 @@ CREATE TABLE IF NOT EXISTS ${DATABASE}.otel_metrics_gauge
     INDEX idx_scope_attr_key mapKeys(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
     INDEX idx_scope_attr_value mapValues(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
     INDEX idx_attr_key mapKeys(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1,
-    INDEX idx_attr_value mapValues(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1
+    INDEX idx_attr_value mapValues(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+    INDEX idx_time_minmax TimeUnix TYPE minmax GRANULARITY 1
 )
 ENGINE = MergeTree
 PARTITION BY toDate(TimeUnix)
-ORDER BY (ServiceName, MetricName, Attributes, toUnixTimestamp64Nano(TimeUnix))
+ORDER BY (ServiceName, MetricName, toStartOfHour(TimeUnix), cityHash64(Attributes), TimeUnix)
 TTL toDateTime(TimeUnix) + ${TABLES_TTL}
 SETTINGS ttl_only_drop_parts = 1;
 
@@ -50,12 +51,12 @@ CREATE TABLE IF NOT EXISTS ${DATABASE}.otel_metrics_sum
     `MetricDescription` String CODEC(ZSTD(1)),
     `MetricUnit` String CODEC(ZSTD(1)),
     `Attributes` Map(LowCardinality(String), String) CODEC(ZSTD(1)),
-    `StartTimeUnix` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
-    `TimeUnix` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
+    `StartTimeUnix` DateTime CODEC(Delta(8), ZSTD(1)),
+    `TimeUnix` DateTime CODEC(Delta(8), ZSTD(1)),
     `Value` Float64 CODEC(ZSTD(1)),
     `Flags` UInt32 CODEC(ZSTD(1)),
     `Exemplars.FilteredAttributes` Array(Map(LowCardinality(String), String)) CODEC(ZSTD(1)),
-    `Exemplars.TimeUnix` Array(DateTime64(9)) CODEC(ZSTD(1)),
+    `Exemplars.TimeUnix` Array(DateTime) CODEC(ZSTD(1)),
     `Exemplars.Value` Array(Float64) CODEC(ZSTD(1)),
     `Exemplars.SpanId` Array(String) CODEC(ZSTD(1)),
     `Exemplars.TraceId` Array(String) CODEC(ZSTD(1)),
@@ -66,11 +67,12 @@ CREATE TABLE IF NOT EXISTS ${DATABASE}.otel_metrics_sum
     INDEX idx_scope_attr_key mapKeys(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
     INDEX idx_scope_attr_value mapValues(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
     INDEX idx_attr_key mapKeys(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1,
-    INDEX idx_attr_value mapValues(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1
+    INDEX idx_attr_value mapValues(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+    INDEX idx_time_minmax TimeUnix TYPE minmax GRANULARITY 1
 )
 ENGINE = MergeTree
 PARTITION BY toDate(TimeUnix)
-ORDER BY (ServiceName, MetricName, Attributes, toUnixTimestamp64Nano(TimeUnix))
+ORDER BY (ServiceName, MetricName, toStartOfHour(TimeUnix), cityHash64(Attributes), TimeUnix)
 TTL toDateTime(TimeUnix) + ${TABLES_TTL}
 SETTINGS ttl_only_drop_parts = 1;
 -- +goose StatementEnd
@@ -90,14 +92,14 @@ CREATE TABLE IF NOT EXISTS ${DATABASE}.otel_metrics_histogram
     `MetricDescription` String CODEC(ZSTD(1)),
     `MetricUnit` String CODEC(ZSTD(1)),
     `Attributes` Map(LowCardinality(String), String) CODEC(ZSTD(1)),
-    `StartTimeUnix` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
-    `TimeUnix` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
+    `StartTimeUnix` DateTime CODEC(Delta(8), ZSTD(1)),
+    `TimeUnix` DateTime CODEC(Delta(8), ZSTD(1)),
     `Count` UInt64 CODEC(Delta(8), ZSTD(1)),
     `Sum` Float64 CODEC(ZSTD(1)),
     `BucketCounts` Array(UInt64) CODEC(ZSTD(1)),
     `ExplicitBounds` Array(Float64) CODEC(ZSTD(1)),
     `Exemplars.FilteredAttributes` Array(Map(LowCardinality(String), String)) CODEC(ZSTD(1)),
-    `Exemplars.TimeUnix` Array(DateTime64(9)) CODEC(ZSTD(1)),
+    `Exemplars.TimeUnix` Array(DateTime) CODEC(ZSTD(1)),
     `Exemplars.Value` Array(Float64) CODEC(ZSTD(1)),
     `Exemplars.SpanId` Array(String) CODEC(ZSTD(1)),
     `Exemplars.TraceId` Array(String) CODEC(ZSTD(1)),
@@ -110,11 +112,12 @@ CREATE TABLE IF NOT EXISTS ${DATABASE}.otel_metrics_histogram
     INDEX idx_scope_attr_key mapKeys(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
     INDEX idx_scope_attr_value mapValues(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
     INDEX idx_attr_key mapKeys(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1,
-    INDEX idx_attr_value mapValues(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1
+    INDEX idx_attr_value mapValues(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+    INDEX idx_time_minmax TimeUnix TYPE minmax GRANULARITY 1
 )
 ENGINE = MergeTree
 PARTITION BY toDate(TimeUnix)
-ORDER BY (ServiceName, MetricName, Attributes, toUnixTimestamp64Nano(TimeUnix))
+ORDER BY (ServiceName, MetricName, toStartOfHour(TimeUnix), cityHash64(Attributes), TimeUnix)
 TTL toDateTime(TimeUnix) + ${TABLES_TTL}
 SETTINGS ttl_only_drop_parts = 1;
 -- +goose StatementEnd
@@ -134,8 +137,8 @@ CREATE TABLE IF NOT EXISTS ${DATABASE}.otel_metrics_exponential_histogram
     `MetricDescription` String CODEC(ZSTD(1)),
     `MetricUnit` String CODEC(ZSTD(1)),
     `Attributes` Map(LowCardinality(String), String) CODEC(ZSTD(1)),
-    `StartTimeUnix` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
-    `TimeUnix` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
+    `StartTimeUnix` DateTime CODEC(Delta(8), ZSTD(1)),
+    `TimeUnix` DateTime CODEC(Delta(8), ZSTD(1)),
     `Count` UInt64 CODEC(Delta(8), ZSTD(1)),
     `Sum` Float64 CODEC(ZSTD(1)),
     `Scale` Int32 CODEC(ZSTD(1)),
@@ -145,7 +148,7 @@ CREATE TABLE IF NOT EXISTS ${DATABASE}.otel_metrics_exponential_histogram
     `NegativeOffset` Int32 CODEC(ZSTD(1)),
     `NegativeBucketCounts` Array(UInt64) CODEC(ZSTD(1)),
     `Exemplars.FilteredAttributes` Array(Map(LowCardinality(String), String)) CODEC(ZSTD(1)),
-    `Exemplars.TimeUnix` Array(DateTime64(9)) CODEC(ZSTD(1)),
+    `Exemplars.TimeUnix` Array(DateTime) CODEC(ZSTD(1)),
     `Exemplars.Value` Array(Float64) CODEC(ZSTD(1)),
     `Exemplars.SpanId` Array(String) CODEC(ZSTD(1)),
     `Exemplars.TraceId` Array(String) CODEC(ZSTD(1)),
@@ -158,11 +161,12 @@ CREATE TABLE IF NOT EXISTS ${DATABASE}.otel_metrics_exponential_histogram
     INDEX idx_scope_attr_key mapKeys(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
     INDEX idx_scope_attr_value mapValues(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
     INDEX idx_attr_key mapKeys(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1,
-    INDEX idx_attr_value mapValues(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1
+    INDEX idx_attr_value mapValues(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+    INDEX idx_time_minmax TimeUnix TYPE minmax GRANULARITY 1
 )
 ENGINE = MergeTree
 PARTITION BY toDate(TimeUnix)
-ORDER BY (ServiceName, MetricName, Attributes, toUnixTimestamp64Nano(TimeUnix))
+ORDER BY (ServiceName, MetricName, toStartOfHour(TimeUnix), cityHash64(Attributes), TimeUnix)
 TTL toDateTime(TimeUnix) + ${TABLES_TTL}
 SETTINGS ttl_only_drop_parts = 1;
 -- +goose StatementEnd
@@ -182,8 +186,8 @@ CREATE TABLE IF NOT EXISTS ${DATABASE}.otel_metrics_summary
     `MetricDescription` String CODEC(ZSTD(1)),
     `MetricUnit` String CODEC(ZSTD(1)),
     `Attributes` Map(LowCardinality(String), String) CODEC(ZSTD(1)),
-    `StartTimeUnix` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
-    `TimeUnix` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
+    `StartTimeUnix` DateTime CODEC(Delta(8), ZSTD(1)),
+    `TimeUnix` DateTime CODEC(Delta(8), ZSTD(1)),
     `Count` UInt64 CODEC(Delta(8), ZSTD(1)),
     `Sum` Float64 CODEC(ZSTD(1)),
     `ValueAtQuantiles.Quantile` Array(Float64) CODEC(ZSTD(1)),
@@ -194,11 +198,12 @@ CREATE TABLE IF NOT EXISTS ${DATABASE}.otel_metrics_summary
     INDEX idx_scope_attr_key mapKeys(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
     INDEX idx_scope_attr_value mapValues(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
     INDEX idx_attr_key mapKeys(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1,
-    INDEX idx_attr_value mapValues(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1
+    INDEX idx_attr_value mapValues(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+    INDEX idx_time_minmax TimeUnix TYPE minmax GRANULARITY 1
 )
 ENGINE = MergeTree
 PARTITION BY toDate(TimeUnix)
-ORDER BY (ServiceName, MetricName, Attributes, toUnixTimestamp64Nano(TimeUnix))
+ORDER BY (ServiceName, MetricName, toStartOfHour(TimeUnix), cityHash64(Attributes), TimeUnix)
 TTL toDateTime(TimeUnix) + ${TABLES_TTL}
 SETTINGS ttl_only_drop_parts = 1;
 -- +goose StatementEnd
